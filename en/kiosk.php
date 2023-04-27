b9affdb845b97a79f6d264779ffe7405721b6c4f
arvin <arvin.duan@mic.com.tw>
2023-04-27 19:48:01 +0800

    add kiosk page

---
+++ en/kiosk.php
@@ -0,0 +1,87 @@
+<?php
+	include 'inc/HEADER.php';
+?>
+
+<!-- 更換banner -->
+
+	<!-- Primary Page Layout
+	================================================== -->
+	<!-- Hero Block
+	================================================== -->
+	<div class="section mid-height-incNav over-hide" id="kiosk_banner">	
+		<div class="parallax" style="background-image: url('../images/dashcam_recorder/dashcam_recorder-banner.jpg')"></div>
+		<div class="hero-center-wrap z-bigger">
+			<div class="container color-white hero-text big-text text-center parallax-fade-top">
+				<div class="row">
+          <!-- <div class="col-md-4 align-self-center">
+            <img class="img-fluid mx-auto d-block rounded" src="../images/motobike_smart_cam/Motobike-Smart-Cam-banner-product.png" alt="Motobike Smart Cam">
+          </div> -->
+					<div class="col-md-12 align-self-center">	
+						<h1 class="color-white mb-xl-5">Self-Service Kiosk</h1>
+            <h3 class="h4">Take Your Business to the Next Level</h3>
+					</div>	
+				</div>		
+			</div>			
+		</div>	
+	</div>
+
+
+  <div class="section padding-top-bottom background-white">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-10">	
+					<div class="call-box-4">
+						<p class="ContGen">MiTAC Self-Service Kiosk is a stylish, highly interactive and state-of-the-art self-service solution that provides modular hardware design with powerful computing platform and industrial-grade reliability. The future-proof kiosk enables intuitive and efficient access to services, reduce wait times greatly and improve the overall customer experience for a wide range of industries including QSR, retail, hospitality, healthcare.</p>
+					</div>
+				</div>
+			</div>	
+		</div>		
+	</div>
+
+  <!-- 更換圖片 -->
+  <div class="section" id="kiosk">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/kiosk/android_security_green.png" alt="Android open platform">
+            <p>Android open platform</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/kiosk/efficient_data.png" alt="Cloud database">
+            <p>Cloud database</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/kiosk/wproof_green.png" alt="Self-scan & checkout">
+            <p>Self-scan & checkout</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/kiosk/power_through.png" alt="Multiple payment options">
+            <p>Multiple payment options</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/kiosk/barcode_data_capture.png" alt="Receipt printing on the go">
+            <p>Receipt printing on the go</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/kiosk/256G-SD-Card.png" alt="24/7 Non-stop operations">
+            <p>24/7 Non-stop operations</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<?php
+	include 'inc/FOOTER.php';
+?>
---
+++ tw/kiosk.php
@@ -0,0 +1,87 @@
+<?php
+	include 'inc/HEADER.php';
+?>
+
+<!-- 更換banner -->
+
+	<!-- Primary Page Layout
+	================================================== -->
+	<!-- Hero Block
+	================================================== -->
+	<div class="section mid-height-incNav over-hide" id="kiosk_banner">	
+		<div class="parallax" style="background-image: url('../images/dashcam_recorder/dashcam_recorder-banner.jpg')"></div>
+		<div class="hero-center-wrap z-bigger">
+			<div class="container color-white hero-text big-text text-center parallax-fade-top">
+				<div class="row">
+          <!-- <div class="col-md-4 align-self-center">
+            <img class="img-fluid mx-auto d-block rounded" src="../images/motobike_smart_cam/Motobike-Smart-Cam-banner-product.png" alt="Motobike Smart Cam">
+          </div> -->
+					<div class="col-md-12 align-self-center">	
+						<h1 class="color-white mb-xl-5">自助服務機</h1>
+            <h3 class="h4">將您的業務提升至更高的水準</h3>
+					</div>	
+				</div>		
+			</div>			
+		</div>	
+	</div>
+
+
+  <div class="section padding-top-bottom background-white">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-10">	
+					<div class="call-box-4">
+						<p class="ContGen">神達數位自助服務機是一個時尚、高度互動且先進的自助服務解決方案，提供模組化硬體設計、強大的演算平台和工業級可靠性。具有未來感的自助服務機提供直覺和高效率的服務，大幅減少顧客等待時間，並改善包括速食業、零售業、觀光業和醫療業等多種產業的整體顧客體驗。</p>
+					</div>
+				</div>
+			</div>	
+		</div>		
+	</div>
+
+  <!-- 更換圖片 -->
+  <div class="section" id="kiosk">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/kiosk/android_security_green.png" alt="Android開放平台">
+            <p>Android開放平台</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/kiosk/efficient_data.png" alt="雲端資料庫">
+            <p>雲端資料庫</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/kiosk/wproof_green.png" alt="自助掃描&結帳">
+            <p>自助掃描&結帳</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/kiosk/power_through.png" alt="多種支付選擇">
+            <p>多種支付選擇</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/kiosk/barcode_data_capture.png" alt="收據列印">
+            <p>收據列印</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/kiosk/256G-SD-Card.png" alt="24/7 不間斷運營">
+            <p>24/7 不間斷運營</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<?php
+	include 'inc/FOOTER.php';
+?>
