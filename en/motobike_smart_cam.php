140d007cbff5306495b4e6359ae82aa15441244b
arvin <arvin.duan@mic.com.tw>
2023-04-27 11:06:55 +0800

    add Motobike Smart Cam page

---
+++ en/motobike_smart_cam.php
@@ -0,0 +1,89 @@
+<?php
+	include 'inc/HEADER.php';
+?>
+
+
+	<!-- Primary Page Layout
+	================================================== -->
+	<!-- Hero Block
+	================================================== -->
+	<div class="section mid-height-incNav over-hide" id="motobike_smart_cam_banner">	
+		<div class="parallax" style="background-image: url('../images/dashcam_recorder/dashcam_recorder-banner.jpg')"></div>
+		<div class="hero-center-wrap z-bigger">
+			<div class="container color-white hero-text big-text text-center parallax-fade-top">
+				<div class="row">
+          <div class="col-md-4 align-self-center">
+            <img class="img-fluid mx-auto d-block rounded" src="../images/motobike_smart_cam/Motobike-Smart-Cam-banner-product.png" alt="">
+          </div>
+					<div class="col-md-8 align-self-center">	
+						<h1 class="color-white mb-xl-5">Motobike Smart Cam</h1>
+            <h3 class="h4">Never miss any important moments while riding</h3>
+					</div>	
+				</div>		
+			</div>			
+		</div>	
+	</div>
+
+
+  <div class="section padding-top-bottom background-white">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-10">	
+					<div class="call-box-4">
+						<p class="ContGen">As a global leader in drive video recorders, MiTAC has developed a new generation of rider cameras that are exceptionally user-friendly, redefining the standard for effortless operation.</p>
+					</div>
+				</div>
+			</div>	
+		</div>		
+	</div>
+
+
+  <div class="section" id="motobike_smart_cam">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/motobike_smart_cam/Dual-Camera-Sync-Record-for-moto.png" alt="Evidence recording">
+            <p>Evidence recording</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/motobike_smart_cam/One-click-Emergency-Recording.png" alt="Event recording">
+            <p>Event recording</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/motobike_smart_cam/One-click Lock.png" alt="One-click for locking event file">
+            <p>One-click for locking event file</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/motobike_smart_cam/Real-time-Event-Backup.png" alt="Video sharing">
+            <p>Video sharing</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/motobike_smart_cam/WIFI_OTA.png" alt="WIFI OTA update">
+            <p>WIFI OTA update</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/motobike_smart_cam/256G-SD-Card.png" alt="Supports up to 256GB SD card">
+            <p>Supports up to 256GB SD card</p>
+          </div>
+        </div>
+        <div class="col-md-12 mt-30 text-center">
+          <a class="btn btn-primary btn-lg btn-round" href="https://www.mio.com/en_eu/products/dash-cameras/rider-camera/mivue-m-series" target="_blank">Learn More</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<?php
+	include 'inc/FOOTER.php';
+?>
---
+++ tw/motobike_smart_cam.php
@@ -0,0 +1,89 @@
+<?php
+	include 'inc/HEADER.php';
+?>
+
+
+	<!-- Primary Page Layout
+	================================================== -->
+	<!-- Hero Block
+	================================================== -->
+	<div class="section mid-height-incNav over-hide" id="motobike_smart_cam_banner">	
+		<div class="parallax" style="background-image: url('../images/dashcam_recorder/dashcam_recorder-banner.jpg')"></div>
+		<div class="hero-center-wrap z-bigger">
+			<div class="container color-white hero-text big-text text-center parallax-fade-top">
+				<div class="row">
+          <div class="col-md-4 align-self-center">
+            <img class="img-fluid mx-auto d-block rounded" src="../images/motobike_smart_cam/Motobike-Smart-Cam-banner-product.png" alt="">
+          </div>
+					<div class="col-md-8 align-self-center">	
+						<h1 class="color-white mb-xl-5">機車行車紀錄器</h1>
+            <h3 class="h4">騎行中不會錯過任何重要時刻</h3>
+					</div>	
+				</div>		
+			</div>			
+		</div>	
+	</div>
+
+
+  <div class="section padding-top-bottom background-white">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-10">	
+					<div class="call-box-4">
+						<p class="ContGen">作為全球行車記錄器的領導者，神達數位開發了新一代機車行車記錄器。輕巧方便易於使用，再次定義了輕鬆操作的標準。</p>
+					</div>
+				</div>
+			</div>	
+		</div>		
+	</div>
+
+
+  <div class="section" id="motobike_smart_cam">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/motobike_smart_cam/Dual-Camera-Sync-Record-for-moto.png" alt="證據錄影">
+            <p>證據錄影</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/motobike_smart_cam/One-click-Emergency-Recording.png" alt="事件錄影">
+            <p>事件錄影</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/motobike_smart_cam/One-click Lock.png" alt="一鍵鎖檔">
+            <p>一鍵鎖檔</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/motobike_smart_cam/Real-time-Event-Backup.png" alt="影片分享">
+            <p>影片分享</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/motobike_smart_cam/WIFI_OTA.png" alt="WIFI OTA無線更新">
+            <p>WIFI OTA無線更新</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/motobike_smart_cam/256G-SD-Card.png" alt="最大支援256GB 記憶卡">
+            <p>最大支援256GB 記憶卡</p>
+          </div>
+        </div>
+        <div class="col-md-12 mt-30 text-center">
+          <a class="btn btn-primary btn-lg btn-round" href="https://www.mio.com/tw/products/category-rider-camera" target="_blank">了解更多</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<?php
+	include 'inc/FOOTER.php';
+?>
