7fac2cfef7a432812f407311c9edfbf8c64b2b90
janet.yang (楊珍妮 - MIH) <janet.yang@mic.com.tw>
2023-04-28 13:28:19 +0800

    add outdoor-navigation

---
+++ en/outdoor-navigation.php
@@ -0,0 +1,93 @@
+<?php
+  include 'inc/HEADER.php';
+?>
+
+<!-- 更換banner -->
+
+	<!-- Primary Page Layout
+	================================================== -->
+	<!-- Hero Block
+	================================================== -->
+	<div class="section mid-height-incNav over-hide" id="outdoor_navigation_banner">	
+		<div class="parallax" style="background-image: url('../images/outdoor-navigation/Outdoor_Navigation-banner.jpg')"></div>
+		<div class="hero-center-wrap z-bigger">
+			<div class="container color-white hero-text big-text text-center parallax-fade-top">
+				<div class="row">
+          <!-- <div class="col-md-4 align-self-center pt-70">
+            <img class="img-fluid mx-auto d-block rounded" src="../images/outdoor-navigation/Outdoor_Navigation-banner-product.png" alt="">
+          </div> -->
+					<div class="col-md-8 align-self-center">	
+						<h1 class="color-white mb-xl-5">Bicycle Navigation</h1>
+            <h3 class="h4">A holistic bike navigation solution for your journey</h3>
+					</div>	
+				</div>		
+			</div>			
+		</div>	
+	</div>
+
+
+  <div class="section padding-top-bottom background-white outdoor_navigation_banner">
+		<div class="container">
+			<div class="row justify-content-left">
+				<div class="col-md-12">	
+					<div class="call-box-8">
+						<p class="ContGen">Compact size with powerful insights, MiTAC Cyclo™ cycling computer allows you to  enjoy the ride with your loved ones and  level up your next trip.</p>
+					</div>
+				</div>
+			</div>	
+		</div>		
+	</div>
+
+  <!-- 更換圖片 -->
+  <div class="section" id="outdoor-navigation">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-navigation-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-navigation/cyclo_icon_NeverMiss.png" alt="NeverMiss™">
+            <p>NeverMiss™</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-navigation-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-navigation/cyclo_icon_turn-by-turn.png" alt="Turn-by-turn guidance">
+            <p>Turn-by-turn guidance</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-navigation-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-navigation/Surpriese.png" alt="Surprise Me™">
+            <p>Surprise Me™</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-navigation-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-navigation/cyclo_icon_eu-map-38.png" alt="Full EU bicycle maps">
+            <p>Full EU bicycle maps</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-navigation-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-navigation/cyclo_icon_MioShare.png" alt="Cloud-based tour management tool">
+            <p>Cloud-based tour management tool</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-navigation-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-navigation/cyclo_icon_routeyou_Komoot_integration.png" alt="3rd party route integration">
+            <p>3rd party route integration</p>
+          </div>
+        </div>
+
+        <div class="col-md-12 mt-60 text-center mb-30">
+          <a class="btn btn-primary btn-lg btn-round" href="https://www.mio.com/en_eu/products/category-outdoors" target="_blank">Learn More</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+  
+<?php
+  include 'inc/FOOTER.php';
+?>
---
+++ tw/outdoor-navigation.php
@@ -0,0 +1,93 @@
+<?php
+  include 'inc/HEADER.php';
+?>
+
+<!-- 更換banner -->
+
+	<!-- Primary Page Layout
+	================================================== -->
+	<!-- Hero Block
+	================================================== -->
+	<div class="section mid-height-incNav over-hide" id="outdoor_navigation_banner">	
+		<div class="parallax" style="background-image: url('../images/outdoor-navigation/Outdoor_Navigation-banner.jpg')"></div>
+		<div class="hero-center-wrap z-bigger">
+			<div class="container color-white hero-text big-text text-center parallax-fade-top">
+				<div class="row">
+          <!-- <div class="col-md-4 align-self-center pt-70">
+            <img class="img-fluid mx-auto d-block rounded" src="../images/outdoor-navigation/Outdoor_Navigation-banner-product.png" alt="">
+          </div> -->
+					<div class="col-md-8 align-self-center">	
+						<h1 class="color-white mb-xl-5">Cyclo自行車導航</h1>
+            <h3 class="h4">全方位的自行車導航解決方案，讓您的旅程更加精采</h3>
+					</div>	
+				</div>		
+			</div>			
+		</div>	
+	</div>
+
+
+  <div class="section padding-top-bottom background-white outdoor_navigation_banner">
+		<div class="container">
+			<div class="row justify-content-left">
+				<div class="col-md-12">	
+					<div class="call-box-8">
+						<p class="ContGen">小巧的體積，強大的功能。神達數位Cyclo自行車導航讓您能夠與您的親友一同享受乘風的樂趣，提升您的騎乘體驗。</p>
+					</div>
+				</div>
+			</div>	
+		</div>		
+	</div>
+
+  <!-- 更換圖片 -->
+  <div class="section" id="outdoor-navigation">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-navigation-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-navigation/cyclo_icon_NeverMiss.png" alt="NeverMiss™ 絕不錯過有趣的景點™">
+            <p>NeverMiss™ 絕不錯過有趣的景點</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-navigation-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-navigation/cyclo_icon_turn-by-turn.png" alt="逐項導航">
+            <p>逐項導航</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-navigation-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-navigation/Surpriese.png" alt="Surprise Me™ 路線推薦">
+            <p>Surprise Me™ 路線推薦</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-navigation-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-navigation/cyclo_icon_eu-map-38.png" alt="全歐洲自行車地圖">
+            <p>全歐洲自行車地圖</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-navigation-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-navigation/cyclo_icon_MioShare.png" alt="雲端旅程管理工具">
+            <p>雲端旅程管理工具</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-navigation-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-navigation/cyclo_icon_routeyou_Komoot_integration.png" alt="第三方路線服務整合">
+            <p>第三方路線服務整合</p>
+          </div>
+        </div>
+
+        <div class="col-md-12 mt-60 text-center mb-30">
+          <a class="btn btn-primary btn-lg btn-round" href="https://www.mio.com/en_eu/products/category-outdoors" target="_blank">Learn More</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+  
+<?php
+  include 'inc/FOOTER.php';
+?>
