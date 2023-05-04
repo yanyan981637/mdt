d6267eacbbc6b3451d66886e370de83c60b79a36
janet.yang (楊珍妮 - MIH) <janet.yang@mic.com.tw>
2023-05-04 17:04:02 +0800

    add auxiliary-software-smart-industrial page

---
+++ en/auxiliary-software-smart-industrial.php
@@ -0,0 +1,173 @@
+<?php
+	include 'inc/HEADER.php';
+?>
+
+<div class="section mid-height-incNav over-hide">	
+		<div class="parallax" style="background-image: url('../images/auxiliary-software-smart-industrial/Auxiliary_Software-banner.jpg')"></div>
+		<div class="hero-center-wrap move-bottom z-bigger">
+			<div class="container color-white hero-text big-text text-center parallax-fade-top">
+				<div class="row">
+					<div class="col-md-12 mg-auto">	
+						<h1 class="color-white mb-xl-3">Auxiliary Software</h1>
+					</div>	
+				</div>		
+			</div>			
+		</div>	
+	</div>
+</div>
+
+<div class="SecTopNavPlaceholder"></div>
+	<div class="SecTopNav">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 hidden-xs">
+					<span><a href="#midm">MiDM™</a></span>
+					<span><a href="#milock">MiLock™</a></span>
+			</div>
+		</div>
+	</div>
+</div>	
+
+	<!-- MiDM™-->
+	<div class="section over-hide" id="midm">
+		<div class="container">
+			<div class="row">	
+				<div class="col-12">
+					<h3 class="h3 mt-30">MiTAC (Mobile) Device Management</h3>
+					<h5>Seamlessly configure, manage, deploy device configurations, files and application or device updates - all remotely.</h5>
+					<p>MiTAC (Mobile) Device Management (MiDM) is a comprehensive and cost-effective MDM suite that enables business to securely deploy  mobile devices in multiple user groups over-the air from a single management console - regardless of geographical location.</p>
+				</div>
+				<div class="col-md-7 mt-20 align-items-stretch">
+					<div class="row">
+						<div class="col-xl-4 col-lg-3 col-md-4 col-sm-2 col-6 mt-30">
+							<div class="auxiliary-software-icon text-center">
+								<img class="rounded img-fluid" src="../images/auxiliary-software-smart-industrial/apps_os_management.png" alt="APPs & OS Management">
+								<p>APPs & OS Management</p>
+							</div>
+						</div>
+
+						<div class="col-xl-4 col-lg-3 col-md-4 col-sm-2 col-6 mt-30">
+							<div class="auxiliary-software-icon text-center">
+								<img class="rounded img-fluid" src="../images/auxiliary-software-smart-industrial/real_time_trouble_shooting.png" alt="Real-time Trouble Shooting ">
+								<p>Real-time Trouble Shooting</p>
+							</div>
+						</div>
+
+						<div class="col-xl-4 col-lg-3 col-md-4 col-sm-2 col-6 mt-30">
+							<div class="auxiliary-software-icon text-center">
+								<img class="rounded img-fluid" src="../images/auxiliary-software-smart-industrial/ensure_productivity.png" alt="Ensure Productivity">
+								<p>Ensure Productivity</p>
+							</div>
+						</div>
+
+		
+						<div class="col-xl-4 col-lg-3 col-md-4 col-sm-2 col-6 mt-30">
+							<div class="auxiliary-software-icon text-center">
+								<img class="rounded img-fluid" src="../images/auxiliary-software-smart-industrial/complete_tracking.png" alt="Complete Tracking">
+								<p>Complete Tracking </p>
+							</div>
+						</div>
+
+						<div class="col-xl-4 col-lg-3 col-md-4 col-sm-2 col-6 mt-30">
+							<div class="auxiliary-software-icon text-center">
+								<img class="rounded img-fluid" src="../images/auxiliary-software-smart-industrial/schedule_planning1.png" alt="Significant Cost Reduction">
+								<p>Significant Cost Reduction</p>
+							</div>
+						</div>
+
+						<div class="col-xl-4 col-lg-3 col-md-4 col-sm-2 col-6 mt-30">
+							<div class="auxiliary-software-icon text-center">
+								<img class="rounded img-fluid" src="../images/auxiliary-software-smart-industrial/schedule_planning.png" alt="Flexible Scheduling">
+								<p>Flexible Scheduling</p>
+							</div>
+						</div>
+
+					</div>
+        </div>
+				<div class="col-md-5 mt-30">
+					<img src="../images/auxiliary-software-smart-industrial/MiDM™-banner.jpg" alt="" class="rounded mx-auto d-block img-fluid auxiliary_software_img">
+			  </div>	
+
+			<div class="col-md-12 mt-30">
+        <a class="btn btn-primary btn-lg btn-round m-3" href="https://enterprise.mio.com/en_eu/midm" target="_blank">Learn More</a>
+				<a class="btn btn-fill-black-b  btn-round m-3" href="https://enterprise.mio.com/en_eu/midm" target="_blank">Apply to Tablet</a>
+
+				<hr class="mt-40">
+      </div>
+
+			
+		</div>	
+	</div>
+
+	
+	<!-- MiLock™ -->
+	<div class="section over-hide portfolio-box" id="#milock">
+		<div class="container">
+			<div class="row">	
+				<div class="col-12">
+					<h2 class="h3 mt-30">MiLock™  App</h2>
+					<h5>Turn your device into Single App Kiosk mode</h5>
+					<p>MiLock™ is a proprietary kiosk mode app developed by MiTAC. It replaces the default Android™ Launcher App, and provides businesses with a tool to enhance employee productivity and device usability. With MiLock, user access can be restricted to a single application in 'kiosk-mode', or to a group of approved applications that will replace the traditional homescreen. MiLock is ideal for scenario's where devices are utilized for specific use cases, such as Fleet Management, Table ordering, Warehousing, Logistics, etc. </p>
+				</div>
+				<div class="col-md-7 mt-20">
+					<div class="row">
+						<div class="col-xl-4 col-lg-3 col-md-4 col-sm-2 col-6 mt-30">
+							<div class="auxiliary-software-icon text-center">
+								<img class="rounded img-fluid" src="../images/auxiliary-software-smart-industrial/single_multi_app_mode.png" alt="Single / Multi App Mode">
+								<p>Single / Multi App Mode</p>
+							</div>
+						</div>
+
+						<div class="col-xl-4 col-lg-3 col-md-4 col-sm-2 col-6 mt-30">
+							<div class="auxiliary-software-icon text-center">
+								<img class="rounded img-fluid" src="../images/auxiliary-software-smart-industrial/restricted_settings_access.png" alt="Restricted Settings Access ">
+								<p>Restricted Settings Access </p>
+							</div>
+						</div>
+
+						<div class="col-xl-4 col-lg-3 col-md-4 col-sm-2 col-6 mt-30">
+							<div class="auxiliary-software-icon text-center">
+								<img class="rounded img-fluid" src="../images/auxiliary-software-smart-industrial/home_screen_customization.png" alt="Home Screen Customization">
+								<p>Home Screen Customization</p>
+							</div>
+						</div>
+
+		
+						<div class="col-xl-4 col-lg-3 col-md-4 col-sm-2 col-6 mt-30">
+							<div class="auxiliary-software-icon text-center">
+								<img class="rounded img-fluid" src="../images/auxiliary-software-smart-industrial/advanced_function_lockdown.png" alt="Advanced Function Lockdown">
+								<p>Advanced Function Lockdown</p>
+							</div>
+						</div>
+
+						<div class="col-xl-4 col-lg-3 col-md-4 col-sm-2 col-6 mt-30">
+							<div class="auxiliary-software-icon text-center">
+								<img class="rounded img-fluid" src="../images/auxiliary-software-smart-industrial/wireless_configuration_deployment.png" alt="Wireless Configuration Deployment">
+								<p>Wireless Configuration Deployment</p>
+							</div>
+						</div>
+
+						<div class="col-xl-4 col-lg-3 col-md-4 col-sm-2 col-6 mt-30">
+							<div class="auxiliary-software-icon text-center">
+								<img class="rounded img-fluid" src="../images/auxiliary-software-smart-industrial/ensure_productivity.png" alt="Enhance Employee Productivity">
+								<p>Enhance Employee Productivity</p>
+							</div>
+						</div>
+
+					</div>
+        </div>
+				<div class="col-md-5 mt-30">
+					<img src="../images/auxiliary-software-smart-industrial/MiLock-banner.jpg" alt="" class="rounded mx-auto d-block img-fluid">
+			  </div>	
+
+			<div class="col-md-12 mt-30">
+				<a class="btn btn-fill-black-b  btn-round m-3" href="https://enterprise.mio.com/en_eu/" target="_blank">Apply to Tablet</a>
+      </div>
+			
+		</div>	
+	</div>
+
+
+<?php
+include 'inc/FOOTER.php';
+?>
