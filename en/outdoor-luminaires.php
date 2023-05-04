a2a62cae9c75a4328d8d76011cc6020a806af1bc
janet.yang (楊珍妮 - MIH) <janet.yang@mic.com.tw>
2023-05-04 15:30:59 +0800

    add outdoor-luminaires page

--- en/outdoor-luminaires.php
+++ en/outdoor-luminaires.php
@@ -1,26 +1,33 @@
 <?php
 	include 'inc/HEADER.php';
 ?>
-<div class="section mid-height-incNav over-hide">	
+
+<!-- 更換banner -->
+
+	<!-- Primary Page Layout
+	================================================== -->
+	<!-- Hero Block
+	================================================== -->
+	<div class="section mid-height-incNav over-hide" id="outdoor_navigation_banner">	
 		<div class="parallax" style="background-image: url('../images/Hero_slider/hero_Smart _IOT.jpg')"></div>
-		<div class="hero-center-wrap move-bottom z-bigger">
+		<div class="hero-center-wrap z-bigger">
 			<div class="container color-white hero-text big-text text-center parallax-fade-top">
 				<div class="row">
-					<div class="col-md-12 mg-auto">	
-						<h1 class="color-white mb-xl-3">Outdoor luminaires</h1>
-						<p class="font_content">Light up your life intelligently </p>
+					<div class="col-md-12 align-self-center">	
+						<h1 class="color-white mb-xl-5">Outdoor luminairy connectivity module</h1>
+            <h3 class="h4">Seamless and convenient connections</h3>
 					</div>	
 				</div>		
 			</div>			
 		</div>	
 	</div>
-	</div>
-	
-	<div class="section padding-top-bottom background-white">
+
+
+  <div class="section padding-top-bottom background-white outdoor_navigation_banner">
 		<div class="container">
 			<div class="row justify-content-center">
-				<div class="col-md-14">	
-					<div class="call-box-4">
+				<div class="col-md-10">	
+					<div class="call-box-8">
 						<p class="ContGen">Equipped with a smart control unit, flexible sensors, and a camera, our location-based outdoor luminaire solution realizes the integration of lighting, monitoring, tracking, and sensing.</p>
 					</div>
 				</div>
@@ -28,73 +35,50 @@
 		</div>		
 	</div>
 
-	<div class="row justify-content-center">
-				<div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6">
-					<div class="qmBox">
-						<img src="../images/certificates/ISO_2002.png" alt="ISO9001認證">
-						<div class="txt">
-							<p>2002</p>
-							<span>ISO9001</span>
-							<span>Ver.2015</span>
-						</div>
-					</div>
-				</div>
+  <!-- 更換圖片 -->
+  <div class="section" id="outdoor-luminaires">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-luminaires-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-luminaires/Location_based.png" alt="Location based - GPS/GLONASS/BAIDU">
+            <p>Location based - GPS/GLONASS/BAIDU</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-luminaires-icon text-center outdoor-luminaires-second">
+            <img class="rounded img-fluid" src="../images/outdoor-luminaires/Flexible_connectivity.png" alt="Flexible connectivity-4G/3G/2G/LTE/NB-IoT/LoRa">
+            <p>Flexible connectivity -4G/3G/2G/LTE/&nbsp;&nbsp;&nbsp;&nbsp; NB-IoT/LoRa</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-luminaires-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-luminaires/Lighting.png" alt="Lighting">
+            <p>Lighting</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-luminaires-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-luminaires/Monitoring.png" alt="Monitoring">
+            <p>Monitoring</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-luminaires-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-luminaires/Tracking.png" alt="Tracking">
+            <p>Tracking</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-luminaires-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-luminaires/Sensing.png" alt="Sensing">
+            <p>Sensing</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 				
-				<div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6">
-					<div class="qmBox">
-						<img src="../images/certificates/ISO_2003.png" alt="ISO14001認證">
-						<div class="txt">
-							<p>2003</p>
-							<span>ISO14001</span>
-							<span>Ver.2015</span>
-						</div>
-					</div>
-				</div>
-				
-				<div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6">
-					<div class="qmBox">
-						<img src="../images/certificates/ISO_2005.png" alt="OHSAS18001認證">
-						<div class="txt">
-							<p>2005</p>
-							<span>OHSAS18001</span>
-							<span>Ver.2007</span>
-						</div>
-					</div>
-				</div>
-				
-				<div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6">
-					<div class="qmBox">
-						<img src="../images/certificates/ISO_2007.png" alt="ISO/IATF16949認證">
-						<div class="txt">
-							<p>2007</p>
-							<span>ISO/IATF16949</span>
-							<span>Ver.2016</span>
-						</div>
-					</div>
-				</div>
-				
-				<div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6">
-					<div class="qmBox">
-						<img src="../images/certificates/ISO_2012.png" alt="ISO13485認證">
-						<div class="txt">
-							<p>2012</p>
-							<span>ISO13485</span>
-							<span>Ver.2016</span>
-						</div>
-					</div>
-				</div>
-				
-				<div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6">
-					<div class="qmBox">
-						<img src="../images/certificates/ISO_2016.png" alt="G.M.P認證">
-						<div class="txt">
-							<p>2016</p>
-							<span>G.M.P (TW)</span>
-							<span>Ver.2015</span>
-						</div>
-					</div>
-				</div>
-			</div>
 <?php
 	include 'inc/FOOTER.php';
 ?>
--- tw/outdoor-luminaires.php
+++ tw/outdoor-luminaires.php
@@ -1,34 +1,84 @@
 <?php
 	include 'inc/HEADER.php';
 ?>
-<div class="section mid-height-incNav over-hide">	
+
+<!-- 更換banner -->
+
+	<!-- Primary Page Layout
+	================================================== -->
+	<!-- Hero Block
+	================================================== -->
+	<div class="section mid-height-incNav over-hide" id="outdoor_navigation_banner">	
 		<div class="parallax" style="background-image: url('../images/Hero_slider/hero_Smart _IOT.jpg')"></div>
-		<div class="hero-center-wrap move-bottom z-bigger">
+		<div class="hero-center-wrap z-bigger">
 			<div class="container color-white hero-text big-text text-center parallax-fade-top">
 				<div class="row">
-					<div class="col-md-12 mg-auto">	
-						<h1 class="color-white mb-xl-3">戶外照明</h1>
-						<p class="font_content">聰明地點亮你的生活</p>
+					<div class="col-md-12 align-self-center">	
+						<h1 class="color-white mb-xl-5">戶外照明連網模組</h1>
+            <h3 class="h4">無縫且便捷的連網</h3>
 					</div>	
 				</div>		
 			</div>			
 		</div>	
 	</div>
-	</div>
 
-	<div class="section padding-top-bottom background-white">
+
+  <div class="section padding-top-bottom background-white outdoor_navigation_banner">
 		<div class="container">
 			<div class="row justify-content-center">
-				<div class="col-md-14">	
-					<div class="call-box-4">
+				<div class="col-md-10">	
+					<div class="call-box-8">
 						<p class="ContGen">配備了智慧控制元件、彈性靈活的傳感器和攝影機，我們的定位型戶外照明解決方案實現了照明、監控、追蹤和感測的整合。</p>
 					</div>
 				</div>
 			</div>	
 		</div>		
 	</div>
-	
-</div>			
+
+  <!-- 更換圖片 -->
+  <div class="section" id="outdoor-luminaires">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-luminaires-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-luminaires/Location_based.png" alt="定位-GPS/GLONASS/BAIDU">
+            <p>定位-GPS/GLONASS/BAIDU</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-luminaires-icon text-center outdoor-luminaires-second">
+            <img class="rounded img-fluid" src="../images/outdoor-luminaires/Flexible_connectivity.png" alt="彈性的連線 - 4G/3G/2G/LTE/NB-IoT/LoRa">
+            <p>彈性的連線 - 4G/3G/2G/LTE/ <span style="display:block"> NB-IoT/LoRa</span></p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-luminaires-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-luminaires/Lighting.png" alt="照明">
+            <p>照明</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-luminaires-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-luminaires/Monitoring.png" alt="監控">
+            <p>監控</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-luminaires-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-luminaires/Tracking.png" alt="追蹤">
+            <p>追蹤</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="outdoor-luminaires-icon text-center">
+            <img class="rounded img-fluid" src="../images/outdoor-luminaires/Sensing.png" alt="感測">
+            <p>感測</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+				
 <?php
 	include 'inc/FOOTER.php';
 ?>
