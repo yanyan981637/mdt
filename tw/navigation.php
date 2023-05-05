76283fb531c2cda4c713d3f434fb4970d4d09882
janet.yang (楊珍妮 - MIH) <janet.yang@mic.com.tw>
2023-05-05 15:54:21 +0800

    add navigation page

--- tw/navigation.php
+++ tw/navigation.php
@@ -1,90 +1,171 @@
 <?php
 	include 'inc/HEADER.php';
 ?>
+
+
 	<!-- Primary Page Layout
 	================================================== -->
 	<!-- Hero Block
 	================================================== -->
 	<div class="section mid-height-incNav over-hide">	
-		<div class="parallax" style="background-image: url('../images/Hero_slider/hero_navigation.jpg')"></div>
+		<div class="parallax" style="background-image: url('../images/dashcam_recorder/dashcam_recorder-banner.jpg')"></div>
 		<div class="hero-center-wrap z-bigger">
 			<div class="container color-white hero-text big-text text-center parallax-fade-top">
 				<div class="row">
-					<div class="col-md-12 mg-auto">	
-						<h1 class="color-white mb-xl-5">導航解決方案</h1>
+					<div class="col-md-12">	
+						<h1 class="color-white mb-xl-5">導航系統</h1>
 					</div>	
 				</div>		
 			</div>			
-		</div>		
-	</div>
-
-
-	<div class="section padding-top-bottom-small background-white">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-md-10">	
-					<div class="call-box-4">
-						<p class="ContGen">
-							神達數位累積多年在GPS領域的專業技術，提供全方位GPS導航解決方案，範圍涵蓋一般道路導航、汽車越野導航、車隊管理導航、商用及公用車隊往返路徑管理解決方案。我們提供多樣化且專業的導航解決方案，無論是一般使用者或商業用途夥伴，都可以獲得滿足。
-						</p>
-					</div>
-				</div>
-			</div>	
-		</div>		
-	</div>
-
-
-
-
-	<div class="section">
-		<div class="container">
-			<img src="../images/solution/navigation.jpg" alt="MiTAC Digital Technology Corp. develop AIOT / Smart City business" style="width:100%;">
-		</div>
-	</div>
-	
-
-	<div class="section over-hide pt_3r" id="connected_car">
-		<div class="container">
-			<div class="row">	
-				<div class="col-md-6 align-items-stretch padding-top-small background-grey" style="padding-bottom: 3rem;">
-						<div class="productGrid section justify-content-center text-left rwd-padding-on-grid">
-							<h2 class="FC_mdt1">導航解決方案</h2>
-						</div>	
-				</div>
-				<div class="col-md-6 align-items-stretch padding-top-small background-grey hidden-xs"></div>	
-			</div>
-			<div class="row">	
-				<div class="col-md-6 align-items-stretch padding-bottom-small background-grey">	
-					<div class="productGrid section justify-content-center text-left rwd-padding-on-grid">
-						<div class="clear"></div>
-						<div class="section text-left">	
-							<div class="txtBox">	
-								<h3 class="h5">主要應用</h3>
-								<p class="st1">汽車導航解決方案</p>
-								<p class="st1">汽車越野導航解決方案</p>
-								<p class="st1">使用者可直接從車內儀表板規劃完美行車路徑</p>
-								<p class="st1">車隊導航 &amp; 商用及公用車隊返回路徑解決方案</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-6 align-items-stretch padding-bottom-small background-grey">	
-					<div class="productGrid section justify-content-center text-left rwd-padding-on-grid">
-						<div class="clear"></div>
-						<div class="section text-left">	
-							<div class="txtBox">	
-								<h3 class="h5">產品線</h3>
-								<p class="st1">車用導航軟體</p>
-								<p class="st1">車隊導航軟體與商用及公用車隊返回路徑解決方案</p>
-								<span class="more mt-30"><a href="https://www.magellangps.com/magellan-trx-off-road-gps-navigation" target="_blank">了解更多 <i class="fa fa-external-link" aria-hidden="true"></i></a></span>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>	
 		</div>	
 	</div>
 
+	<div class="SecTopNavPlaceholder"></div>
+	<div class="SecTopNav">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 hidden-xs">
+					<span><a href="#car-navigation">汽車導航</a></span>
+					<span><a href="#off-road-navigation">越野導航</a></span>
+				</div>
+			</div>
+		</div>
+	</div>
+
+  <div class="section padding-top-bottom background-white">
+		<div class="container">
+			<div class="row justify-content-center">
+			   <div class="col-md-12 mt-30">
+            <p>我們可靠且全方位的GPS 導航系統能夠消除行車時的不確定性因素。無論您是上下班通勤還是越野旅程，我們專為汽車設計的 GPS導航系統能夠讓您輕鬆到達目的地。</p>
+        </div>
+			</div>	
+		</div>		
+	</div>
+  <!-- Car Navigation -->
+  <div class="section" id="car-navigation">
+    <div class="second-bannner">
+      <div class="bg-img" style="background-image: url('../images/dashcam_recorder/singlecam-banner.jpg')">
+        <div class="container text-center color-white center">
+          <div class="row">
+            <div class="align-self-center col-md-12 mg-auto">	
+              <h2>汽車導航</h2>
+              <h4>自信地駕駛,更快到達目的地</h4>
+            </div>	
+          </div>		
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 mt-30">
+            <p>神達數位汽車導航系統搭配高效GPS導航軟體，配備先進的路徑規劃算法，能快速、正確地計算出您的行駛路線且提供簡單清楚的視覺和語音引導，幫助您輕鬆到達目的地。</p>
+        </div>
+
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+            <div class="dashcam-recorder-icon text-center">
+              <img class="rounded img-fluid" src="../images/navigation/Lifetime_Map_Update.png" alt="終身地圖更新">
+              <p>終身地圖更新</p>
+            </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/navigation/Turn_by_Turn_Navigation.png" alt="先進且功能強大的逐項導航">
+            <p>先進且功能強大的逐項導航</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/navigation/Choice_of_Routes.png" alt="多種路徑選擇">
+            <p>多種路徑選擇</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/navigation/Traffic_Detour_Information.png" alt="交通流量資訊">
+            <p>交通流量資訊</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/navigation/Trip_Planner.png" alt="旅程規劃">
+            <p>旅程規劃</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/navigation/Vehicle _racking.png" alt="LearnMe Pro™  客製化路線規劃">
+            <p>LearnMe Pro™  客製化路線規劃</p>
+          </div>
+        </div>
+        <div class="col-md-12 mt-30 text-center">
+          <a class="btn btn-primary btn-lg btn-round" href="https://www.mio.com/en_eu/products/navigation/car-navigation" target="_blank">了解更多</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Off Road Navigation -->
+  <div class="section mt-10vh" id="off-road-navigation">
+    <div class="second-bannner">
+      <div class="bg-img" style="background-image: url('../images/dashcam_recorder/multi-views-DVR-banner.jpg')">
+        <div class="container text-left color-white center">
+          <div class="row">
+            <div class="align-self-center col-md-12 mg-auto">	
+              <h2>越野導航</h2>
+              <h4>越野GPS導航帶您探索更多可能性</h4>
+            </div>	
+          </div>		
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 mt-30">
+            <p>神達數位越野導航為您提供一切必要的內容及逐步導航，讓您自信地探索越野小徑或市區街道。我們的TRX和TR系列導航機預裝了完整且高解析的美國地圖和超過6百萬個景點。我們的TRX Trailhead是美國最大的路徑數據庫，包括超過160,000條路徑，並且仍在持續更新擴大中。</p>
+        </div>
+
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+            <div class="dashcam-recorder-icon text-center">
+              <img class="rounded img-fluid" src="../images/navigation/Voice_Guidance.png" alt="逐項路徑 &街道導航預先安裝路徑">
+              <p>逐項路徑 &街道導航預先安裝路徑</p>
+            </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/navigation/150000_OHV_Trails.png" alt="超過160,000條路徑">
+            <p>超過160,000條路徑</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/navigation/Crowdsourced_Track_Database.png" alt="存檔 & 旅程分享">
+            <p>存檔 & 旅程分享</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/navigation/Rear_5MP_AF_Camera.png" alt="內建自動啟動錄影">
+            <p>內建自動啟動錄影</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/navigation/5__7__TOUCH_SCREEN.png" alt="5'' & 7'' 觸控螢幕,易於閱讀">
+            <p>5'' & 7'' 觸控螢幕,易於閱讀</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/navigation/NO_DATA_CONNECTION.png" alt="路徑無需資料連線">
+            <p>路徑無需資料連線</p>
+          </div>
+        </div>
+        <div class="col-md-12 mt-30 text-center">
+          <a class="btn btn-primary btn-lg btn-round" href="#" target="_blank">了解更多</a>
+        </div>
+      </div>
+    </div>
+  </div>
 
 <?php
 	include 'inc/FOOTER.php';
