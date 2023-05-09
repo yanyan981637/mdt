cc5d40ed0b84c5d6fd1e24f73ee3a73e559b3202
janet.yang (楊珍妮 - MIH) <janet.yang@mic.com.tw>
2023-05-09 14:29:55 +0800

    add vt-solution page

---
+++ en/vt-solution.php
@@ -0,0 +1,32 @@
+<?php
+	include 'inc/HEADER.php';
+?>
+
+<div class="section">
+  <div class="embed-responsive embed-responsive-16by9">
+		<video controls="controls"> 
+			<source src="https://www.youtube.com/watch?v=q2Z9ys2_hA4"></source>
+		</video>
+	</div>
+</div>
+
+<!-- <div class="section">
+	<div class="container text-center">
+		<div class="row">
+		  <div class="col-md-12 mt-30">
+			  <h2>VisionMax™</h2>
+				<h5 class="mt-30">Powerful Panoramic Perception  for Leading Safety and Security</h5>
+				<div class="col-md-12 mt-30 text-center">
+          <a class="btn btn-primary  btn-round" href="https://staging.visionmaxfleet.com/login" target="_blank">
+						<img src="../images/smart_telematics_solution/box-arrow-in-right.svg" alt="box-arrow-in-right" width="24" height="24">
+						&nbsp;&nbsp;
+						User Login</a>
+        </div>
+			<div >
+		</div>
+	</div>
+</div> -->
+
+<?php
+	include 'inc/FOOTER.php';
+?>
---
+++ tw/solution.php
@@ -0,0 +1,174 @@
+<?php
+	include 'inc/HEADER.php';
+?>
+
+
+	<!-- Primary Page Layout
+	================================================== -->
+	<!-- Hero Block
+	================================================== -->
+	<div class="section mid-height-incNav over-hide">	
+		<div class="parallax" style="background-image: url('../images/smart_telematics_solution/Solution-banner.jpg')"></div>
+		<div class="hero-center-wrap z-bigger">
+			<div class="container color-white hero-text big-text text-center parallax-fade-top">
+				<div class="row">
+					<div class="col-md-12 align-self-center">	
+						<h1 class="color-white mb-xl-5">車載資訊解決方案</h1>
+					</div>	
+				</div>		
+			</div>			
+		</div>	
+	</div>
+
+  <div class="SecTopNavPlaceholder"></div>
+	<div class="SecTopNav hidden-xs">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<span><a href="#vt-solution">VT Solution-VisionMax™</a></span>
+					<span><a href="#routecomplete">RouteComplete™</a></span>
+         </div>
+			</div>
+		</div>
+	</div>
+
+
+  <div class="section padding-top-bottom background-white">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-12">	
+					<p>神達數位車載資訊解決方案協助車隊提升安全、效率和永續發展性。能讓依賴實體運作的企業有效利用遠端監控資料來獲取有意義的商業洞見，並優化營運。</p>
+				</div>
+			</div>	
+		</div>		
+	</div>
+
+
+  <div class="section" id="vt-solution">
+    <div class="second-bannner">
+      <div class="bg-img" style="background-image: url('../images/smart_telematics_solution/VT_Solution-banner.jpg')">
+        <div class="container text-center color-white center">
+          <div class="row">
+            <div class="align-self-center col-md-12 mg-auto">	
+              <h2>VisonMax™</h2>
+              <h4>強大的全景監控、領先的安全保障</h4>
+            </div>	
+          </div>		
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 mt-30">
+            <p>VisionMax著重於駕駛安全事件監控及資料管理存儲，提供第一時間安全事件提醒及事後安全事件重建並建立強大的安全系統，以確保駕駛及車隊獲的最佳安全保障及全天候防護。</p>
+        </div>
+
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+            <div class="dashcam-recorder-icon text-center">
+              <img class="rounded img-fluid" src="../images/smart_telematics_solution/4k-ultra-hd-2160p.png" alt="穩定連線傳輸">
+              <p>穩定連線傳輸</p>
+            </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/smart_telematics_solution/HDR.png" alt="多路即時錄影">
+            <p>多路即時錄影</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/smart_telematics_solution/WIFI_OTA.png" alt="可信賴及穩定的平台">
+            <p>可信賴及穩定的平台</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/smart_telematics_solution/6-in-1-speedcam-alert.png" alt="提供有效解決方案">
+            <p>提供有效解決方案</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/smart_telematics_solution/icon_supermp4.png" alt="路線即時監控">
+            <p>路線即時監控</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/smart_telematics_solution/Wide-Angle-Camera.png" alt="線上軟體更新 (OTA)">
+            <p>線上軟體更新 (OTA)</p>
+          </div>
+        </div>
+        <div class="col-md-12 mt-30 text-center">
+          <a class="btn btn-primary btn-lg btn-round" href="#" target="_blank">Learn More</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="section mt-10vh" id="routecomplete">
+    <div class="second-bannner">
+      <div class="bg-img" style="background-image: url('../images/smart_telematics_solution/Return_to_Route-banner.jpg')">
+        <div class="container text-center color-white center">
+          <div class="row">
+            <div class="align-self-center col-md-12 mg-auto">	
+              <h2>路徑返回</h2>
+              <h4>路線規劃, 管理, 監控與執行</h4>
+            </div>	
+          </div>		
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 mt-30">
+            <p>使用神達數位 RTR解決方案，您可以克服最具挑戰性的安全、成本和服務問題——此解決方案專門為在多變天氣條件下運行的鏟雪車隊和撒鹽車隊而設計。</p>
+        </div>
+
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+            <div class="dashcam-recorder-icon text-center">
+              <img class="rounded img-fluid" src="../images/smart_telematics_solution/Locked_On_Route.png" alt="鎖定路線">
+              <p>鎖定路線</p>
+            </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/smart_telematics_solution/Deviated_Point_Navigation.png" alt="偏移點導航">
+            <p>偏移點導航</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/smart_telematics_solution/event_driven_rerounting.png" alt="事件驅動重新路線規劃">
+            <p>事件驅動重新路線規劃</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/smart_telematics_solution/high_accuracy_gps.png" alt="高精準度 GPS">
+            <p>高精準度 GPS</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/smart_telematics_solution/real_time_route_completion _monitoring.png" alt="即時路徑完成監控">
+            <p>即時路徑完成監控</p>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 mt-30">
+          <div class="dashcam-recorder-icon text-center">
+            <img class="rounded img-fluid" src="../images/smart_telematics_solution/real_time_route .png" alt="即時路徑管理">
+            <p>即時路徑管理</p>
+          </div>
+        </div>
+        
+        <div class="col-md-12 mt-30 text-center">
+          <a class="btn btn-primary btn-lg btn-round" href="https://www.magellangps.com/fleet/return-to-route-solution-for-snow" target="_blank">Learn More</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<?php
+	include 'inc/FOOTER.php';
+?>
