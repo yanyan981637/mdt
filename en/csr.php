ce791b155cc346889a7c3917f77dc92dfd6d95da
janet <janet.yang@mic.com.tw>
2023-06-21 09:08:57 +0800

    csr page

--- en/csr.php
+++ en/csr.php
@@ -22,7 +22,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-12">
-					<h2>Corporate Social Responsibility Rooted in Mio's Belief – Safety</h2>
+					<h3>Corporate Social Responsibility Rooted in Mio's Belief – Safety</h3>
 					<p>MiTAC is a world-class leader in automotive electronics, and safety is our core value. We strive to provide products and services that improve safe driving through innovative design, high-standard inspection, and manufacturing. Our offerings include 4K ultra-high-definition/starlight-grade night vision driving recorders, Advanced Driver Assistance Systems, six-in-one safety camera warnings, and driver behavior analysis.  In the future, we will continue to deepen our innovation and quality strength in the field of smart IoT and automotive electronics. We aim to establish a leading position in the industry ecosystem and bring safe and convenient interconnected living to people.</p>
 				</div>
 			</div>
@@ -33,7 +33,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-12">
-					<h2>Establishing a Sustainable Company Full of Happiness</h2>
+					<h3>Establishing a Sustainable Company Full of Happiness</h3>
 					<p>We value talent and are committed to creating a positive and friendly working environment that encourages employees to challenge themselves and achieve their goals. Our passionate, innovative, and responsible professional team transforms technology into services that meet customer needs, providing forward-looking and complete solutions that are key to gaining the trust of our partners. By creating a win-win situation for customers, employees, and the company, we aim to establish a happy and sustainable enterprise that contributes to the stable growth, profits, and sustainable development of society and the environment. 'F.I.R.S.T.' is the principle we follow to achieve our goals.</p>
 				</div>
 			</div>
@@ -45,7 +45,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-12">
-					<h2>Green Product</h2>
+					<h3>Green Product</h3>
 					<p>Faced with increasingly severe challenges of global warming and energy and resource depletion, MiTAC is committed to product eco-design to reduce the environmental impact of our products. We base our design principles on international environmental protection laws and regulations, as well as customer requirements. Our eco-design approach includes energy conservation, materials reduction, ease of disassembly, and waste recovery simplification. To control hazardous chemical substances, we set short- and medium-term goals for their reduction, and strive to reduce environmental impacts during the manufacturing process. We have established an environmental management system and various environmental management SOPs to control waste production, as well as energy and material consumption during the manufacturing process. </p>
 					<img src="../images/csr/CSR_image_en.jpg" class="img-fluid rounded mx-auto d-block">
 				</div>
@@ -53,51 +53,211 @@
 		</div>
 	</div>
 
+  <div class="section mt-60 csr-content">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<h3>MDT ESG Items</h3>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="mt-30 col-md-6 ">
+					<h4>Package materials</h4>
+					<ul>
+						<li>Comply with the Packaging and Packaging Waste Directive (94/62/EC).</li>
+						<li>Packaging materials that do not contain PVC.</li>
+						<li>EPE (extended polyethylene) materials are used for cushioning instead of EPS (extended polystyrene) materials.</li>
+						<li>Packaging materials are shared, and about 90% of the packaging designs can be shared.</li>
+						<li>Adjusting the space for accessory design can increase the total number of products that can be placed in a single box.</li>
+						<li>Replace the complete manual printing with a single Quick Guide and provide customers with an electronic version of the user manual for download.</li>
+						<li>Implement optimization designs for packaging according to different requirements for sea and air transportation, increasing the weight that a single pallet can carry.</li>
+						<li>80% of the outer packaging material is made from recycled pulp, while the inner packaging material does not use any coating and adopts non-toxic, recyclable and environmentally friendly materials.</li>
+					</ul>
+				</div>
+				<div class="mt-30 col-md-6 ">
+					<img src="../images/csr/img_eco-friendly_product_strategies.jpg" alt="" class="img-fluid rounded mx-auto d-block">
+				</div>
+			</div>
+		</div>
+	</div>
+
 	<div class="section mt-60 csr-content">
 		<div class="container">
 			<div class="row">
 				<div class="col-12">
-					<h2>MDT ESG Items</h2>
-					<h5>Package materials</h5>
+					<h4>Product & package materials purchasing strategy</h4>
 					<ul>
-						<li>Comply with the Packaging and Packaging Waste Directive (94/62/EC)</li>
-						<li>Packaging materials that do not contain PVC </li>
-						<li>EPE (extended polyethylene) materials are used for cushioning instead of EPS (extended polystyrene) materials</li>
-						<li>Packaging materials are shared, and about 90% of the packaging designs can be shared. </li>
-						<li>Adjusting the space for accessory design can increase the total number of products that can be placed in a single box.</li>
-						<li>Replace the complete manual printing with a single Quick Guide and provide customers with an electronic version of the user manual for download.</li>
-						<li>Implement optimization designs for packaging according to different requirements for sea and air transportation, increasing the weight that a single pallet can carry.</li>
-						<li>80% of the outer packaging material is made from recycled pulp, while the inner packaging material does not use any coating and adopts non-toxic, recyclable and environmentally friendly materials</li>
-					</ul>
-					<h5>Product & package materials purchasing strategy</h5>
-					<ul>
-						<li>All products comply with the Restriction of Hazardous Substances (RoHS) recast Directive 2011/65/EU and the RoHS recast Directive 2015/863/EU. </li>
+						<li>All products comply with the Restriction of Hazardous Substances (RoHS) recast Directive 2011/65/EU and the RoHS recast Directive 2015/863/EU.</li>
 						<li>Meet the design requirements of the Waste Electrical and Electronic Equipment (WEEE) recast Directive 2012/19/EU.</li>
 						<li>Over 80% of the raw materials used in our products are recycled and reused.</li>
 					</ul>
-					<h5>Customer satisfaction survey</h5>
+				</div>
+        <div class="col-12 table-responsive mt-30">
+					<table class="table table-bordered">
+						<tbody class="text-center">
+						  <tr class="table-mian-color">
+								<td rowspan="2" class="csr-table-heade">Product Name</td>
+								<td colspan="3">Weight</td>
+								<td rowspan="2" class="csr-table-heade">Recycled materials %</td>
+							</tr>
+							<tr class="table-mian-color">								
+								<td>Product</td>
+								<td>Recycled materials</td>
+								<td>Non-recycled materials</td>								
+							</tr>
+							<tr class="table-bc1">	
+								<td>Dashcam - N678</td>
+								<td>168.96</td>
+								<td>152.26</td>
+								<td>16.7</td>
+								<td>90%</td>
+							</tr>
+							<tr class="table-bc2">	
+								<td>Dashcam - N695</td>
+								<td>400.46</td>
+								<td>373.83</td>
+								<td>26.63</td>
+								<td>93%</td>
+							</tr>
+							<tr class="table-bc1">	
+								<td>Dashcam - N695</td>
+								<td>137.69</td>
+								<td>121.26</td>
+								<td>16.43</td>
+								<td>88%</td>
+							</tr>
+							<tr class="table-bc2">	
+								<td>Navigation - N650</td>
+								<td>564.62</td>
+								<td>513.8</td>
+								<td>50.78</td>
+								<td>91%</td>
+							</tr>							
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="section mt-60 ">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<h4>Customer satisfaction survey</h4>
 					<ul>
 						<li>Set up a dedicated backend customer service and repair team at headquarters and upgraded online customer service backend management system - ZOHO. This not only effectively improves customer satisfaction but also saves manpower management expenses and costs.</li>
-						<li>Customer satisfaction survey for after-sales service and repair services has consistently achieved a satisfaction rate of 80% or higher.</li>
+						<li>Customer satisfaction survey for after-sales service and repair services has consistently achieved a satisfaction rate of 80%or higher</li>
+						<li>Customer satisfaction is the core value of MiTAC in perpetual corporate development. The routine VOC evaluation (quarterly/semi-annually, or annually) is essential for improvement. MiTAC makes ceaseless efforts in continued improvement on the basis of the feedback of consumers in the market. The feedback will be taken as the indicators of product design, which includes quality, technology, price, delivery.</li>
 					</ul>
-					<h5>Public welfare sponsored activities</h5>
+				</div>
+				<div class="col-12">
+					<img src="../images/csr/img_customer_satisfaction_survey.png" alt="" class="img-fluid rounded mx-auto  d-none d-md-none d-md-block">
+				</div>
+				<!-- 手机端 -->
+				<div style="position: relative;">
+				  <img src="../images/csr/img_customer_satisfaction_survey_m.png" class_alias="d-lg-block"  alt="" class="img-fluid 	d-block d-sm-none">
+					<img src="../images/csr/img_customer_satisfaction_survey_m.png" class_alias="d-lg-block"  alt="" class="img-fluid 	d-none d-sm-block d-md-none" >
+			  </div>
+			</div>
+		</div>
+	</div>
+
+	<div class="section mt-30 csr-content">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 mt-30">
+          <h4>Public welfare sponsored activities</h4>
 					<ul>
-						<li>Sponsored Taipei Standard Chartered Marathon for five consecutive years</li>
-						<li>Donated Mio Dashcam to the Guishan Branch of the National Police Agency in Taiwan</li>
-						<li>Sponsored golf events in Philippines</li>
-						<li>Sponsored solar-powered water resource projects in Philippines</li>
+						<li>Sponsored Taipei Standard Chartered Marathon for five consecutive years.</li>
+						<li>Donated Mio Dashcam to the Guishan Branch of the National Police Agency in laiwan.</li>
+						<li>Sponsored golf events in Philippines.</li>
+						<li>Sponsored solar-powered water resource projects in Philippines.</li>
+						<li>Long-term sponsorship to the Chinese Taipei Disabled Baseball Association.</li>
+					</ul>
+				</div>
+				<div class="col-md-6 mt-30">
+					<img src="../images/csr/Mio_sponser.jpg" alt="" class="img-fluid rounded mx-auto d-block">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="section mt-60 ">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+				<h4>Talent attraction and development</h4>
+					<ul>
+						<li>Provide diverse learning resources to create an environment conducive to individual strengths.</li>
+						<li>Conduct training programs for new employees to accelerate their integration into the organization.</li>
+						<li>Offer management training to enhance communication and leadership skills.</li>
+						<li>Encourage employees to pursue further education and assist in career development.</li>
+						<li>Implement a comprehensive performance management system, retirement plans, and employee benefits.</li>
+						<li>Promote digital transformation learning to enhance digital competitiveness.</li>
 					</ul>
 				</div>
 			</div>
 		</div>
 	</div>
 
+	<div class="section mt-30" style="background-color:#eaeef4;">
+	  <div class="container csr">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="card topColor1">
+						<div class="card-body">
+							<h5 class="card-title">Transformation study group</h5>			
+							<div class="text-center csr_des">
+								<img src="../images/csr/ico_5_modules.png" alt="ico_5_modules">
+								<span>5 modules</span>
+							</div>
+							<div class="csr_content mt-20">
+							  <p>Why digital transformation is necessary</p>
+								<p>The core challenges of digital transformation</p>
+								<p>5 directions of digital transformation</p>
+								<p>3 approaches to digital transformation</p>
+								<p>Best practices for businesses</p>
+							</div>											
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-6">
+					<div class="card topColor2">
+						<div class="card-body">
+						  <h5 class="card-title">Workshop</h5>
+							<div class="text-center csr_des">
+								<img src="../images/csr/ico_6_topics.png" alt="ico_6_topics">
+								<span>6 topics</span>
+							</div>
+							<div class="csr_content mt-20">
+								<p>Innovation and growth</p>
+								<p>Customer experience</p>
+								<p>Manufacturing and supply chain management</p>
+								<p>Technology and information applications</p>
+								<p>Corporate governance</p>
+								<p>Organization and personnel</p>
+							</div>							
+						</div>
+					</div>
+				  
+				</div>
+			</div>
+		</div>
+		
+	</div>
+
+
+
 
 	<div class="section mt-60 csr-content">
 		<div class="container">
 			<div class="row">
 				<div class="col-12">
-					<h2>Download MiTAC sustainability report</h2>
+					<h3>Download MiTAC sustainability report</h3>
 					<p>
 						<a class="btn btn-blue1 btn-lg btn-round" href="/Downloads/csr/MiTAC_Sustainability_2021_EN_Final-AA1000-update.pdf" target="_blank">2021</a>
 						<a class="btn btn-blue1 btn-lg btn-round" href="/Downloads/csr/MiTAC_CSR_2020_TW_final_Eng.pdf" target="_blank">2020</a>
--- tw/csr.php
+++ tw/csr.php
@@ -22,8 +22,8 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-12">
-					<h2>企業社會責任，深植神數/Mio的信念 - 安全</h2>
-					<p>神達數位是世界級的車用電子領導者, ”安全”是我們的核心價值，我們以創新的設計以及高標準檢驗與製造等自我要求，提供提高安全駕駛的產品與服務, 如4K超高畫質/星光級夜視行車紀錄器、 ADAS先進駕駛輔助系統、安全預警六合一、駕駛行為分析等。未來將持續深耕智慧物聯、車用電子領域的創新能力與品質實力，奠定在產業生態鏈上的領先地位，帶給人們安全便利的互聯生活。</p>
+					<h3>企業社會責任，深植神數/Mio的信念 - 安全</h3>
+					<p>神達數位是世界級的車用電子領導者，”安全”是我們的核心價值，我們以創新的設計以及高標準檢驗與製造等自我要求，提供提高安全駕駛的產品與服務，如4K超高畫質/星光級夜視行車紀錄器、 ADAS先進駕駛輔助系統、安全預警六合一、駕駛行為分析等。未來將持續深耕智慧物聯、車用電子領域的創新能力與品質實力，奠定在產業生態鏈上的領先地位，帶給人們安全便利的互聯生活。</p>
 				</div>
 			</div>
 		</div>
@@ -33,7 +33,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-12">
-					<h2>建立充滿幸福感的永續企業</h2>
+					<h3>建立充滿幸福感的永續企業</h3>
 					<p>我們重視人才並且致力於打造正向積極、友善的工作環境，鼓勵員工勇於挑戰自我、實現成就。 熱情、創新並且勇於承擔的專業團隊，將技術轉化為貼近需求的服務，提供客戶前瞻、完整的解決方案是我們獲得合作夥伴信任的重要關鍵， 創造客戶、員工與企業三贏，建立穩定成長獲利、對社會與環境永續發展有所貢獻的幸福企業， 『F.I.R.S.T.』則是我們實現目標的法則。</p>
 				</div>
 			</div>
@@ -45,7 +45,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-12">
-					<h2>產品環境友善化策略</h2>
+					<h3>產品環境友善化策略</h3>
 					<p>面對全球暖化與能資源匱乏挑戰日益嚴峻，為降低產品對環境的衝擊，神達產品設計就環境友善性為考量，依據國際環保法規及客戶要求，在研發設計將節能、材料減量化、產品的易拆解和簡化廢棄回收等列入產品設計；針對有害化學物質管控，設定有害物質減量短、中期目標；減少製造過程對環境的衝擊，建立環境管理系統，明訂各項環境管理的作業流程，以落實管控在製造過程中所產生廢棄物與能資源使用。</p>
 					<img src="../images/csr/CSR_image_ch.jpg" class="img-fluid rounded mx-auto d-block">
 				</div>
@@ -57,36 +57,27 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-12">
-					<h2>神數ESG施行項目</h2>
-					<h5>包裝材料</h5>
+					<h3>神數ESG施行項目</h3>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="mt-30 col-md-6 ">
+					<h4>包裝材料</h4>
 					<ul>
-						<li>符合包材與包材廢棄物指令 (Packaging and Packaging Waste Directive, 94/62/EC)</li>
-						<li>導入不含 PVC 之包材</li>
+						<li>符合包材與包材廢棄物指令 (Packaging and Packaging Waste Directive，94/62/EC)。</li>
+						<li>導入不含 PVC 之包材。</li>
 						<li>緩衝材採 EPE (extended polyethylene) 材質取代環境危害較大之 EPS (extended polystyrene) 材質。</li>
 						<li>包裝採共用材料，約 90% 的包材設計皆可共用，不但降低備料，並節省採購成本及運輸費用與燃料的使用。</li>
 						<li>運輸包裝減量，分別針對海空運不同需求，透過包裝最佳化設計，提高單位棧板可承載產品重量。</li>
-						<li>設計調整配件之空間，可增加單箱產品放置的總數量</li>
-						<li>改以單張 Quick Guide 取代完整手冊印刷，提供客戶電子版使用手冊下載</li>
-						<li>外包材 80% 採用回收紙漿; 內包材不使用任何塗料，並採用無毒可回收的環保材質</li>
+						<li>設計調整配件之空間，可增加單箱產品放置的總數量。</li>
+						<li>改以單張 Quick Guide 取代完整手冊印刷，提供客戶電子版使用手冊下載。</li>
+						<li>外包材 80% 採用回收紙漿; 內包材不使用任何塗料，並採用無毒可回收的環保材質。</li>
 					</ul>
-					<h5>核心產品銷售（本體）包裝之採購策略</h5>
-					<ul>
-						<li>所有產品符合電氣及電子設備使用某些危害物質限制重鑄指令 (RoHS recast Directive, 2011/65/EU)及RoHS委託修訂指令(2015/863/EU)</li>
-						<li>依廢電機電子設備重鑄指令 (WEEE recast directive, 2012/19/EU) 要求進行產品設計</li>
-						<li>產品所使用之原物料達 80% 以上回收再利用率</li>
-					</ul>
-					<h5>客戶滿意度調查</h5>
-					<ul>
-						<li>Mio品牌於總部設置專職品牌後端客服、維修單位, 並升級線上客服管理平台 ,有效提升品牌的客戶滿意度，且節省人力管理的支出及成本。</li>
-						<li>自2021年第一季啟動品牌自主定期消費者客服滿意度調查，售後客服以及維修服務滿意度皆達到80%以上。</li>
-					</ul>
-					<h5>公益贊助活動</h5>
-					<ul>
-						<li>連續五年贊助台北渣打馬拉松</li>
-						<li>捐贈龜山分局Mio行車記錄器</li>
-						<li>菲律賓高爾夫球活動贊助</li>
-						<li>菲律賓當地太陽能水資源贊助案</li>
-					</ul>
+				</div>
+				<div class="mt-30 col-md-6 ">
+					<img src="../images/csr/img_eco-friendly_product_strategies_ch.jpg" alt="" class="img-fluid rounded mx-auto d-block">
 				</div>
 			</div>
 		</div>
@@ -96,7 +87,174 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-12">
-					<h2>下載神達永續報告書</h2>
+					<h4>核心產品銷售（本體）包裝之採購策略</h4>
+					<ul>
+						<li>所有產品符合電氣及電子設備使用某些危害物質限制重鑄指令 (RoHS recast Directive，2011/65/EU)及RoHS委託修訂指令(2015/863/EU)。</li>
+						<li>依廢電機電子設備重鑄指令 (WEEE recast directive，2012/19/EU) 要求進行產品設計。</li>
+						<li>產品所使用之原物料達 80% 以上回收再利用率。</li>
+					</ul>
+				</div>
+        <div class="col-12 table-responsive mt-30">
+					<table class="table table-bordered">
+						<tbody class="text-center">
+						  <tr class="table-mian-color">
+								<td rowspan="2" class="csr-table-heade">產品名稱</td>
+								<td colspan="3">重量(公克)</td>
+								<td rowspan="2" class="csr-table-heade">可再生物料占比</td>
+							</tr>
+							<tr class="table-mian-color">								
+								<td>產品</td>
+								<td>可再生物料</td>
+								<td>不可再生物料</td>								
+							</tr>
+							<tr class="table-bc1">	
+								<td>行車記錄器 - N678</td>
+								<td>168.96</td>
+								<td>152.26</td>
+								<td>16.7</td>
+								<td>90%</td>
+							</tr>
+							<tr class="table-bc2">	
+								<td>行車記錄器 - N695</td>
+								<td>400.46</td>
+								<td>373.83</td>
+								<td>26.63</td>
+								<td>93%</td>
+							</tr>
+							<tr class="table-bc1">	
+								<td>行車記錄器 - N695</td>
+								<td>137.69</td>
+								<td>121.26</td>
+								<td>16.43</td>
+								<td>88%</td>
+							</tr>
+							<tr class="table-bc2">	
+								<td>車用導航 - N650</td>
+								<td>564.62</td>
+								<td>513.8</td>
+								<td>50.78</td>
+								<td>91%</td>
+							</tr>							
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="section mt-60 ">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<h4>客戶滿意度調查</h4>
+					<ul>
+					<li>Mo品牌於總部設置專職品牌後端客服、維修單位，並升級線上客服管理平台，有效提升品牌的客戶滿意度，且節省人力管理的支出及成本。</li>
+					<li>自2021年第一季啟動品牌自主定期消費者客服滿意度調查，售後客服以及維修服務滿意度皆達到80%以上。</li>
+					<li>客戶滿意是神數永續經營的核心價值，定期（季/半年/年度）的客戶滿意度評比VOC(Voice of Customer)是重要的改善依據，根據市場中消費者的回饋信息，持續修正作業流程，視為產品設計指標，内容包括品質、技術、價格、交期等，確保產品能夠符合客戶的需求和期望，進而提升客戶滿意度。</li>
+					</ul>
+				</div>
+				<div class="col-12">
+					<img src="../images/csr/img_customer_satisfaction_survey_ch.png" alt="" class="img-fluid rounded mx-auto  d-none d-md-none d-md-block">
+				</div>
+				<!-- 手机端 -->
+				<div style="position: relative;">
+				  <img src="../images/csr/img_customer_satisfaction_survey_m_ch.png" class_alias="d-lg-block"  alt="" class="img-fluid 	d-block d-sm-none">
+					<img src="../images/csr/img_customer_satisfaction_survey_m_ch.png" class_alias="d-lg-block"  alt="" class="img-fluid 	d-none d-sm-block d-md-none" >
+			  </div>
+			</div>
+		</div>
+	</div>
+
+	<div class="section mt-30 csr-content">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 mt-30">
+          <h4>公益贊助活動</h4>
+					<ul>
+					<li>連續五年贊助台北渣打馬拉松。</li>
+					<li>捐贈龜山分局Mo行車記錄器。</li>
+					<li>菲律賓高爾夫球活動贊助。</li>
+					<li>菲律賓當地太陽能水資源贊助案。</li>
+					<li>長期贊助「社團法人中華民國身障棒壘球協會」。</li>
+					</ul>
+				</div>
+				<div class="col-md-6 mt-30">
+					<img src="../images/csr/Mio_sponser.jpg" alt="" class="img-fluid rounded mx-auto d-block">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="section mt-60 ">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+				<h4>人才吸引與發展</h4>
+					<ul>
+					<li>多元學習資源，打造適才環境。</li>
+					<li>新進人員培訓，加速融入組織。</li>
+					<li>管理職能培訓，提升溝通領導力。</li>
+					<li>鼓勵在職進修，協助職涯發展。</li>
+					<li>完善的績效管理制度，退休計畫與員工福利。</li>
+					<li>數位轉型學習，提升數位競爭力。</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="section mt-30" style="background-color:#eaeef4;">
+	  <div class="container csr">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="card topColor1">
+						<div class="card-body">
+							<h5 class="card-title">轉型讀書會</h5>			
+							<div class="text-center csr_des">
+								<img src="../images/csr/ico_5_modules.png" alt="ico_5_modules">
+								<span>五大模組</span>
+							</div>
+							<div class="csr_content mt-20">
+							  <p>為何須要數位轉型</p>
+								<p>數位轉型的核心挑戰</p>
+								<p>數位轉型五方向</p>
+								<p>數位轉型三方法</p>
+								<p>企業最佳實踐</p>
+							</div>											
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-6">
+					<div class="card topColor2">
+						<div class="card-body">
+						  <h5 class="card-title">研討工作坊</h5>
+							<div class="text-center csr_des">
+								<img src="../images/csr/ico_6_topics.png" alt="ico_6_topics">
+								<span>六大議題</span>
+							</div>
+							<div class="csr_content mt-20">
+								<p>創新與成長</p>
+								<p>客戶體驗</p>
+								<p>製造及供應鏈管理</p>
+								<p>技術與資訊應用</p>
+								<p>公司治理</p>
+								<p>組織與人員</p>
+							</div>							
+						</div>
+					</div>
+				  
+				</div>
+			</div>
+		</div>
+		
+	</div>
+
+	<div class="section mt-60 csr-content">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<h3>下載神達永續報告書</h3>
 					<p>
 						<a class="btn btn-blue1 btn-lg btn-round" href="/Downloads/csr/MiTAC_Sustainability_2021_EN_Final-AA1000-update.pdf" target="_blank">2021</a>
 						<a class="btn btn-blue1 btn-lg btn-round" href="/Downloads/csr/MiTAC_CSR_2020_TW_final_Eng.pdf" target="_blank">2020</a>
