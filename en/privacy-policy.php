2ea8dc0ab84ae2f8c37f000193cc3dffea339d2b
arvin <arvin.duan@mic.com.tw>
2023-07-27 10:03:38 +0800

    reedit privacy policy page

--- en/privacy-policy.php
+++ en/privacy-policy.php
@@ -19,19 +19,19 @@
 		
 			<div class="row policyCont">
 				<div class="col-md-12 mt-30">
-					<p>This privacy policy explains how MiTAC Digital Technology Corporation (‘‘MDT’’ or ‘‘We’’) uses personal data from and about you, on the portal site operated by MDT, by phone, email, or otherwise when you visit our website, or use the services provided on and through our website, including purchasing goods or providing after-sales services.</p>
-					<p>MDT is committed to protecting your personal data and being transparent about what information it holds. MDT understands its obligations to help you understand how and why MDT processes your personal data. This policy tells you about the why, what, how, and when in relation to MDT’s collection and processing of your personal data.</p>
-					<h5>PURPOSE AND BASIS OF MDT’S COLLECTION AND PROCESSING OF YOUR PERSONAL DATA</h5>
-					<p>We collect and process your personal data for purpose of performing our obligations under the membership agreement and/or purchase agreement with you.</p>
-					<p>We use the personal data collected from you for purpose of delivering our membership service to you, completing purchase orders, facilitating payments, rendering of repair and maintenance services, and, upon your consent, providing you with updated news and marketing information on the products and services offered by MDT.</p>
+					<p>This privacy policy explains how MiTAC Digital Technology Corporation (‘‘MiTAC’’ or ‘‘We’’) uses personal data from and about you, on the portal site operated by MiTAC, by phone, email, or otherwise when you visit our website, or use the services provided on and through our website, including purchasing goods, services or providing after-sales services.</p>
+					<p>MiTAC is committed to protecting your personal data and being transparent about what information it holds. MiTAC understands its obligations to help you understand how and why MiTAC processes your personal data. This policy tells you about the why, what, how, and when in relation to MiTAC’s collection and processing of your personal data.</p>
+					<h5>PURPOSE AND BASIS OF MITAC’S COLLECTION AND PROCESSING OF YOUR PERSONAL DATA</h5>
+					<p>We collect and process your personal data for purpose of performing our obligations under the membership agreement, license agreement and/or purchase agreement with you.</p>
+					<p>We use the personal data collected from you for purpose of delivering our service to you, completing purchase orders, facilitating payments, rendering of necessary support, repair and/or maintenance services, and, upon your consent, providing you with updated news and marketing information on the products and services offered by MiTAC.</p>
 					<p>We also use your personal data to respond to you when you contact us.</p>
 					<p>&nbsp;</p>
-					<h5>TYPE OF DATA MDT COLLECTS ABOUT YOU</h5>
+					<h5>TYPE OF DATA MITAC COLLECTS ABOUT YOU</h5>
 					<p>The types of personal information we collect and process about you include:</p>
 					<ul class="list-style">
 						<li><i class="fa fa-caret-right" aria-hidden="true"></i>Your name and contact information (e.g., email, shipping address, and phone number(s));</li>
 						<li><i class="fa fa-caret-right" aria-hidden="true"></i>Billing details such as credit card number and other data necessary for processing payments and fraud prevention, including security code numbers and other related billing information;</li>
-						<li><i class="fa fa-caret-right" aria-hidden="true"></i>Business information details such as information provided in the course of the contractual relationship between you and MDT, or otherwise voluntarily provided by you;</li>
+						<li><i class="fa fa-caret-right" aria-hidden="true"></i>Business information details such as information provided in the course of the contractual relationship between you and MiTAC, or otherwise voluntarily provided by you;</li>
 						<li><i class="fa fa-caret-right" aria-hidden="true"></i>Information that is collected  when you visit on of our website such as cookies;</li>
 						<li><i class="fa fa-caret-right" aria-hidden="true"></i>Information classified as “sensitive” personal data e.g. relating to your health. This information will only be collected and used where it is needed to provide the product or service you have requested or to comply with our legal obligations;</li>
 						<li><i class="fa fa-caret-right" aria-hidden="true"></i>Persistent identifier such as a customer number or other information held in a cookie or IP address; and</li>
@@ -43,34 +43,33 @@
 						<li>A “Cookie” is a small file, typically of letters and numbers, downloaded on to a device (such as desktop, laptop, tablet) when the user accesses certain websites. Cookies are then sent back to the originating website on each subsequent visit. Cookies are useful because they allow a website to recognize a user’s device. The information below explains the cookies we use on our website and why we use them:</li>
 						<li>Cookies and similar technologies allow us to provide you with, for example, customized information from our Website and to make our Website more user-friendly through customer recognition, understanding and facilitation of the customer browsing and shopping behavior. This may include, for example, remembering your language selection so you don’t have to re-enter it every time you visit our Website.</li>
 						<li>In addition, cookies allow us to understand which pages you have visited and to determine how frequently particular pages are visited and to determine the most popular areas of our website. This enables us to improve our website and apps to offer the best shopping experience for you.</li>
-						<li>Our website also employs cookies and similar technologies placed by our carefully selected third party partners, including but not limited to Google and AddThis, to manage our commercial relationship with those partners.</li>
+						<li>Our website also employs cookies and similar technologies placed by our carefully selected third party partners, including but not limited to Google, to manage our commercial relationship with those partners.</li>
 						<li>You can enable or disable cookies by modifying the settings in your browser. However, to make full use of our website, cookies need to be enabled on your browser or device. If you choose not to allow cookies you will not be able to browse parts of our website. Without cookies you will not be able to place an order as cookies are required for the ordering process to be completed. They help, for example, to remember which and how many items you have selected to purchase.</li>
 						<li>We have a variety of pages on social networking sites. If you use these pages, the hosting sites may store cookies onto your device. We do not control the setting of, and accept no liability in connection with these cookies. Please look at the third-party websites for more information about the cookies they use and how you can manage them. Please read the terms of the applicable application for more information.</li>
-						<li>Third party cookies are those that are set by a domain different to the one that the user is visiting. MDT uses some carefully selected third parties to provide certain services to improve the service we offer to you. Some of these third parties use cookies. Information about how we use third parties and what cookies they use and why, is given below.</li>
-						<li>MDT also provides links to external third party websites and you may choose to click through to these sites. If you do so, these third party sites may send cookies to your browser. We do not control the setting of, and accept no liability in connection with, these cookies. Please look at the cookies policies on the respective websites for more information about what they do with cookies and how you can manage them.</li>
+						<li>Third party cookies are those that are set by a domain different to the one that the user is visiting. MiTAC uses some carefully selected third parties to provide certain services to improve the service we offer to you. Some of these third parties use cookies. Information about how we use third parties and what cookies they use and why, is given below.</li>
+						<li>MiTAC also provides links to external third party websites and you may choose to click through to these sites. If you do so, these third party sites may send cookies to your browser. We do not control the setting of, and accept no liability in connection with, these cookies. Please look at the cookies policies on the respective websites for more information about what they do with cookies and how you can manage them.</li>
 						<li>Below list includes cookies deployed by such third parties on our Website.</li>
 					</ol>
 					
 					<h5><i class="fa fa-caret-right" aria-hidden="true"></i> Google Analytics</h5>
-					<p>At MDT, we use Google Analytics to help us understand how our customers navigate to and through our Website. These cookies enable the function of Google analytics. This service helps us understand how long customers spend visiting different pages and how often they return to our Website. Google Analytics also helps our marketing colleagues to work out the effectiveness of our digital marketing campaigns. You can find out how to opt out of these cookies here: <a href="https://tools.google.com/dlpage/gaoptout" target="_blank">https://tools.google.com/dlpage/gaoptout</a></p>
-					<h5><i class="fa fa-caret-right" aria-hidden="true"></i> Addthis</h5>
-					<p>AddThis is providing social media services at the Website through AddThis Buttons. Via these buttons they collect Non-Personally Identifiable information to enable online ad targeting and to provide analytics to MDT. The opt-out cookie is being placed by AddThis on your computer when you choose to opt-out from their services. The Opt-out cookie tells AddThis not to use your information for delivering relevant online advertisements. AddThis allows partners to set cookies and web beacons via certain AddThis Buttons. Some of them are also members of the Network Advertising Initiative (NAI).</p>
+					<p>At MiTAC, we use Google Analytics to help us understand how our customers navigate to and through our Website. These cookies enable the function of Google analytics. This service helps us understand how long customers spend visiting different pages and how often they return to our Website. Google Analytics also helps our marketing colleagues to work out the effectiveness of our digital marketing campaigns. You can find out how to opt out of these cookies here: <a href="https://tools.google.com/dlpage/gaoptout" target="_blank">https://tools.google.com/dlpage/gaoptout</a></p>
+					<p>To know more information about Cookie, please see <a href="cookie-pocily.php">Cookie Policy</a>.</p>
 					
-					<p>To know more information about Cookie, please see <a href="cookie-policy.php">Cookie Policy</a>.</p>
 					<h5>SHARING OF YOUR PERSONAL INFORMATION</h5>
+
 					<ol>
-						<li>Your personal data collected by MDT may be shared with MDT International Corp. and MDT Digital Technology Corp. and companies within MDT group so as to allow MDT to carry out the purposes stated above.</li>
-						<li>MDT is required to disclose certain personal data collected from you to third parties e.g.  logistic service provider(s), maintenance service provider(s), email delivery service provider(s), marketing service provider(s) and cloud service provider(s) we have chosen to support us  for carrying out the purposes stated above.</li>
+						<li>Your personal data collected by MiTAC may be shared with MiTAC International Corp. and companies within MiTAC group so as to allow MiTAC to carry out the purposes stated above.</li>
+						<li>MiTAC is required to disclose certain personal data collected from you to third parties e.g.  logistic service provider(s), maintenance service provider(s), email delivery service provider(s), marketing service provider(s) and cloud service provider(s) we have chosen to support us  for carrying out the purposes stated above.</li>
 						<li>With your prior consent, we may share information about you for publicity and marketing purposes online, in print and on social media.</li>
-						<li>We reassure you that, MDT will, under no circumstances, sell your personal data to third parties or permit third parties to sell the data we have shared with them.</li>
+						<li>We reassure you that, MiTAC will, under no circumstances, sell your personal data to third parties or permit third parties to sell the data we have shared with them.</li>
 					</ol>
 					
-					<h5>Transferring Personal Data outside the Country</h5>
-					<p>Where data needs to be transferred outside the UK, MDT will ensure that all such transfers are in compliance with the terms of the Data Protection Laws and the relevant EU regulations. Where transfers are to be made outside of the European Economic Area, MDT will ensure that the designation country(ies) has data protection laws assessed as adequate by the European Commission or where adequate safeguards are in place.</p>
+					<h5>TRANSFERRING PERSONAL DATA OUTSIDE THE COUNTRY</h5>
+					<p>Where data needs to be transferred outside the country, MiTAC will ensure that all such transfers are in compliance with the terms of the applicable laws and the relevant regulations. Where transfers are to be made outside of the country, MiTAC will ensure that the designation country(ies) has data protection laws assessed as adequate by the country or where adequate safeguards are in place.</p>
 					
 					<h5>YOUR RIGHTS</h5>
 					<ol>
-						<li>Right to withdraw consent. Where MDT is collecting or/and processing data based on your consent, you have the right to withdraw that consent at any time.</li>
+						<li>Right to withdraw consent. Where MiTAC is collecting or/and processing data based on your consent, you have the right to withdraw that consent at any time.</li>
 						<li>
 							In addition, you have the right to:
 							<ul class="list-style">
@@ -84,7 +83,7 @@
 							</ul>
 						</li>
 						<li>We may use automated processing with your personal data to allow us to provide you with service and product information that is most suited for your interests. If you are unhappy with the automated decision, you can contact us and request a manual review.</li>
-						<li>You have the right to lodge a complaint with the UK Information Commissioner should you believe that MDT has violated your privacy right through inappropriate use of your personal data and has failed to promptly cure such violation upon receipt of your request for cure.</li>
+						<li>You have the right to lodge a complaint with the authority should you believe that MiTAC has violated your privacy right through inappropriate use of your personal data and has failed to promptly cure such violation upon receipt of your request for cure.</li>
 					</ol>
 					
 					<h5>CHILDREN'S PRIVACY</h5>
@@ -92,22 +91,28 @@
 					
 					<h5>SECURITY OF YOUR PERSONAL INFORMATION</h5>
 					<ol>
-						<li>MDT is committed to protecting the information you provide us. We take the information and system security very seriously and we strive to comply with our obligations at all times. MDT will take great care in maintaining the security of your personal data and in preventing unauthorized access to it through the use of appropriate technology and internal safeguards.  Your information is protected by controls designed to minimize loss or damage through accident, negligence or deliberate actions.</li>
+						<li>MiTAC is committed to protecting the information you provide us. We take the information and system security very seriously and we strive to comply with our obligations at all times. MiTAC will take great care in maintaining the security of your personal data and in preventing unauthorized access to it through the use of appropriate technology and internal safeguards.  Your information is protected by controls designed to minimize loss or damage through accident, negligence or deliberate actions.</li>
 						<li>We suggest that you do not share your password information with anyone. If you are sharing a computer with others you should always choose to log out before leaving a site or service to protect access to your information from subsequent users.</li>
 					</ol>
 					
 					<h5>DATA RETENTION PERIOD</h5>
 					<ol>
-						<li>Except as otherwise required by applicable law, MDT endeavors to retain your personal data only for as long as is necessary for MDT to fulfill the aforementioned purposes. After the data retention period, we will destroy or delete your personal information or make it anonymous such that it cannot be identified or traced back to you.</li>
+						<li>Except as otherwise required by applicable law, MiTAC endeavors to retain your personal data only for as long as is necessary for MiTAC to fulfill the aforementioned purposes. After the data retention period, we will destroy or delete your personal information or make it anonymous such that it cannot be identified or traced back to you.</li>
 						<li>If in the future we intend to process your personal data for a purpose other than that which it was collected, we will notify you of that purpose together with any other relevant information.</li>
 					</ol>
 					
 					<h5>CHANGES TO THIS PRIVACY POLICY</h5>
-					<p>MDT may change the terms of the Privacy Policy from time to time. In case of any changes, we will post those changes. When we change the policy in a material way, a notice will be posted on our website along with the updated Privacy Policy.</p>
+					<p>MiTAC may change the terms of the Privacy Policy from time to time. In case of any changes, we will post those changes. When we change the policy in a material way, a notice will be posted on our website along with the updated Privacy Policy.</p>
 					
 					<h5>Contact Us</h5>
-					<p>If you have any concerns as to how your personal data is collected or processed or your rights, or any question about our Privacy Policy, you could contact us via the link below.</p>
-					<p><a href="http://www.mitacmdt.com/en/general-inquiry.php">Contact us</a></p>
+					<p>If you have any concerns as to how your personal data is collected or processed or your rights, or any question about our Privacy Policy, you can contact us.</p>
+					<p>MiTAC Digital Technology Corporation</p>
+					<address>
+					4F, No. 1, R&D 2nd Road <br/>
+					Hsinchu Science Park <br/>
+					Hsinchu, Taiwan <br/>
+					Email: <a href="mailto:privacy@mitac.com" target="_blank">privacy@mitac.com</a>
+					</address>
 				</div>
 			</div>
 			
