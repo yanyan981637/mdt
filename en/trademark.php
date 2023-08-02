88d9c3e3f7e7b15278ae01e9527eba21850f94c3
arvin <arvin.duan@mic.com.tw>
2023-08-02 09:27:50 +0800

    re-edit trademark page

--- en/trademark.php
+++ en/trademark.php
@@ -19,13 +19,17 @@
 		
 			<div class="row policyCont">
 				<div class="col-md-12 mt-30">
-					<p>Mio is a name synonymous with high-quality handheld computing and satellite navigation. Mio and other trademarks of MiTAC Technology Corporation (“MiTAC”) are very valuable as they represent excellence and consistent quality. This page contains detailed information about how to reference MiTAC’s trademarks in different scenarios.</p>
+					<p>MiTAC and other trademarks of MiTAC International Corporation, an affiliate of MiTAC Digital Technology Corporation, are very valuable as they represent excellence and consistent quality. </p>
+
+					<p>MiTAC, Mio, Mio all about you, Magellan, NAVMAN and any other trademarks, registrations, brands, product or service names, slogans or logos (collectively, “Trademarks”) contained in this website are owned or used under license by MiTAC Holdings Corporation and its subsidiaries and affiliated companies (collectively, “MiTAC”). All Trademarks are the property of their respective owners and shall not be copied, imitated, or used, in whole or in part, without the prior written consent of MiTAC or the applicable owner(s). Ownership of all such Trademarks and the goodwill associated therewith remains with MiTAC or the applicable owner(s). All rights reserved. Reference to any products, services, processes, or other information by trade name, trademark, or otherwise does not constitute or imply endorsement, warranty, sponsorship, or recommendation thereof by MiTAC . </p>
+
+					<p>This page contains detailed information about how to reference MiTAC’s Trademarks in different scenarios.</p>
 
 					<h5>Referencing MiTAC’s Trademarks in Text</h5>
-					<p>Proper use of MiTAC trademarks is important. You may use MiTAC trademarks or product names to refer to MiTAC products and services provided you follow these guidelines. Your use must not mislead consumers as to any MiTAC sponsorship, affiliation, or endorsement of your company, or your products or services.</p>
+					<p>Proper use of MiTAC’s Trademarks is important. You may use MiTAC’s Trademarks or product names to refer to MiTAC products and services provided you follow these guidelines. Your use must not mislead consumers as to any MiTAC sponsorship, affiliation, or endorsement of your company, or your products or services.</p>
 
 					<h5>Referencing MiTAC’s Trademarks in Text</h5>
-					<p>When referencing MiTAC software and products, apply the appropriate trademark symbols in accordance with the list of current MiTAC trademarks.</p>
+					<p>When referencing MiTAC software and products, apply the appropriate trademark symbols in accordance with the list of current MiTAC’s Trademarks.</p>
 
 					
 					<h5>MiTAC Trademarks List</h5>
@@ -96,10 +100,15 @@
 						  <td style="width: 25.5736%; height: 18px;"><span style="font-weight: 400;">&trade; or <sup>&reg;</sup></span></td>
 						  <td style="width: 48.9779%; height: 18px;"><span style="font-weight: 400;">Software</span></td>
 						</tr>
+						<tr style="height: 18px;">
+						  <td style="width: 25.4484%; height: 18px;"><span style="font-weight: 400;">MiSentry</span></td>
+						  <td style="width: 25.5736%; height: 18px;"><span style="font-weight: 400;">&trade; or <sup>&reg;</sup></span></td>
+						  <td style="width: 48.9779%; height: 18px;"><span style="font-weight: 400;">Handheld Device、Vehicle / Motorcycle Device、Software</span></td>
+						</tr>
 					  </tbody>
 					</table>
-					<p>Mio and MiTAC</p>
-					<p>MiTAC sells its product under the Mio brand.</p>
+					<!-- <p>Mio and MiTAC</p>
+					<p>MiTAC sells its product under the Mio brand.</p> -->
 					<h5>Mio Product Names</h5>
 					<p>When writing about products from the Mio brand, the correct way is to use the complete Mio trademark followed by the model number. You can also refer to the product simply by its model number.</p>
 					<p>
@@ -108,13 +117,13 @@
 					Incorrect: Mio&trade; 892D.
 					</p>
 					<h5>Use MiTAC Trademarks as Modifiers</h5>
-					<p>Set MiTAC trademarks apart from other words or nouns they modify. The common way to do this is to capitalize the product name and use the appropriate trademark symbol and appropriate descriptor. You may also underline, italicize, or bold the name. Examples:</p>
+					<p>Set MiTAC’s Trademarks apart from other words or nouns they modify. The common way to do this is to capitalize the product name and use the appropriate trademark symbol and appropriate descriptor. You may also underline, italicize, or bold the name. Examples:</p>
 					<p>
 						Correct: Before you install the MioMap&trade; navigation software...<br>
 						Incorrect: Before installing miomap programs you can...
 					</p>
 					<h5>Do Not Use MiTAC Trademarks in Possessive or Plural Form</h5>
-					<p>MiTAC’s trademarks should never be used in the possessive or plural form but should be introduced as a proper adjective followed by an appropriate descriptor.</p>
+					<p>MiTAC’s Trademarks should never be used in the possessive or plural form but should be introduced as a proper adjective followed by an appropriate descriptor.</p>
 					<p>
 						Correct: It was easy to get there using MioMap&trade; navigation software.<br>
 						Incorrect: He loaded several MioMaps into his handheld device.
@@ -123,19 +132,19 @@
 	
 					<p>You may reference the names of MiTAC products and services on packaging, web sites, and in advertising materials to indicate your product’s compatibility with a specific MiTAC product or service, provided the reference complies with the guidelines herein and the specifications below.</p>
 					<ul>
-						<li>You may use MiTAC trademarks in referential phrases such as "Works with MioMap&trade;", "Runs on Mio MiVue 892D", and "For use with Mio MiVue 892D".</li>
-						<li>Do not use any MiTAC trademarks as your product name, service name, or company name. Real-time traffic alert</li>
-						<li>Do not use product, service, or company names that could be confused with any MiTAC trademark.</li>
+						<li>You may use MiTAC’s Trademarks in referential phrases such as "Works with MioMap&trade;", "Runs on Mio MiVue 892D", and "For use with Mio MiVue 892D".</li>
+						<li>Do not use any MiTAC’s Trademarks as your product name, service name, or company name. Real-time traffic alert</li>
+						<li>Do not use product, service, or company names that could be confused with any MiTAC’s Trademark.</li>
 						<li>Do not mimic or copy MiTAC's product packaging, or advertising.</li>
-						<li>Do not use MiTAC's trademarks more prominently than your product or service name. You should maintain a visual distinction between your company and product name and any MiTAC trademark.</li>
-						<li>Do not create or use any logos that include a MiTAC trademark unless your use is pursuant to a license from MiTAC (for example, do not create your own MioMap logo).</li>
+						<li>Do not use MiTAC's Trademarks more prominently than your product or service name. You should maintain a visual distinction between your company and product name and any MiTAC’s Trademark.</li>
+						<li>Do not create or use any logos that include a MiTAC’s Trademark unless your use is pursuant to a license from MiTAC (for example, do not create your own MioMap logo).</li>
 					</ul>
 					<h5>Logos</h5>
-					<p>Do not use the Mio logo, MioMap logo, or any other MiTAC logos, symbols, or icons on or in connection with products, packaging, manuals, promotional/advertising materials, or web sites for any purpose except when you have written permission from MiTAC.</p>
+					<p>Do not use the MiTAC logo, Mio logo, or any other MiTAC logos, symbols, or icons on or in connection with products, packaging, manuals, promotional/advertising materials, or web sites for any purpose except when you have written permission from MiTAC.</p>
 					<h5>Endorsement or Sponsorship</h5>
-					<p>Do not use any MiTAC trademark in any manner that expresses or implies MiTAC’s affiliation, sponsorship, endorsement, certification, or approval. Do not use any MiTAC trademark in such a manner that it appears MiTAC is legally associated with your company. You must display your company name more prominently than any MiTAC trademark on all materials.</p>
+					<p>Do not use any MiTAC’s Trademarks in any manner that expresses or implies MiTAC’s affiliation, sponsorship, endorsement, certification, or approval. Do not use any MiTAC’s Trademarks in such a manner that it appears MiTAC is legally associated with your company. You must display your company name more prominently than any MiTAC’s Trademark on all materials.</p>
 					<h5>Merchandise Items</h5>
-					<p>Do not manufacture, sell, or give away merchandise items such as T-shirts, mugs, etc. bearing any MiTAC trademark, including logos, except pursuant to an express written permission from MiTAC.</p>
+					<p>Do not manufacture, sell, or give away merchandise items such as T-shirts, mugs, etc. bearing any MiTAC’s Trademark, including logos, except pursuant to an express written permission from MiTAC.</p>
 
 				</div>
 			</div>
