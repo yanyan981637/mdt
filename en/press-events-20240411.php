406ded0d0dcf64670530dad7749dce6b2c5d9fa8
vic.jiang <vic.jiang@mic.com.tw>
2024-04-12 14:30:58 +0800

    modify press pic

---
+++ en/press-events-20240411.php
@@ -0,0 +1,74 @@
+<?php
+	include 'inc/HEADER.php';
+?>
+	<!-- Primary Page Layout
+	================================================== -->
+	<div id="press_events" class="section pgHero" >	
+		<div class="hero-center-wrap move-bottom z-bigger">
+			<div class="container hero-text big-text text-center parallax-fade-top">
+				<div class="row">
+					<div class="col-md-12 mg-auto">	
+						<h1 class="mb-xl-5 FC_mdtB">Press &amp; Events</h1>
+					</div>	
+				</div>		
+			</div>			
+		</div>	
+	</div>
+	
+	
+	<div class="section padding-bottom-small background-white">
+		<div class="container" style="border-top: solid 4px #313139;">
+		
+			<div class="row newsCont">
+				<div class="col-lg-2 col-md-2 col-sm-2">
+					<span class="h4">Press</span>
+					<p class="dat">2024.04.11</p>
+				</div>
+				<div class="col-lg-10 col-md-10 col-sm-10">
+					<h4 class="mb-30">MiTAC and CANGO Mobility Announce Collaboration to Transform Fleet Management with Integrated Video Telematics and CANbus Data Insight</h4>
+					<!--start-->
+					<div class="row">
+						<div class="col-md-12">
+							<img src="../images/press_news/MiTAC_CANGO.jpg" style="max-width:100%;" />
+						</div>
+						<div class="col-md-12">
+							<p>CANGO Mobility, a leading provider of advanced vehicle telematics solutions, and MiTAC Digital Tehnology Corp. (MDT), a pioneer in Automative technology, are thrilled to announce their strategic collaboration to deliver cutting-edge video telematics solutions for fleet managers worldwide.</p>
+							<p  class="mt_4">MDT's Connected Dashcam – the MioEYE K Series offers an all-in-one connected fleet camera solution, empowering fleet managers with forward-thinking video telematics capabilities. With a focus on enhancing safety, efficiency, and productivity, MDT's solution seamlessly integrates with CANGO's Canbus library, unlocking a wealth of vehicle data insights that drive operational excellence.</p>
+							<p  class="mt_4 b">Explore the benefits of utilizing Video telematics data with CANlib; Uncover the insights that lead to excellence.</p>
+							<p  class="mt_4">CANGO's Canbus library enables fleet managers to access a wide array of vehicle data, providing critical insights into <span class="b">vehicle performance and driver behavior.</span> By making use of this dataset fleet managers can enhance fuel efficiency monitor vehicle status and improve fleet management practices. Some key data points included in CANGOs dataset are odometer readings, vehicle performance metrics, fuel details, state of charge (SOC) state of health (SOH) operating hours and more.</p>
+							<p  class="mt_4">Additionally, integrated with <span class="b">big data, artificial intelligence (AI),</span> and <span class="b">Internet of Things (IoT)</span> technology, MDT’s connected dashcam recorder offers recording from up to seven channels, it allows the capture of specific events such as turns, ignition status, and more, providing a holistic view of fleet operations in real-time.  The MioEYE K Series also provides an open API and SDK, allowing customers to easily integrate it into various platforms based on their needs and application scenarios. The Recorded data can be synchronized and stored either locally on an SD card or in the cloud, offering additional data security and reliability. Users have the flexibility to choose between local storage or cloud backup based on their preferences.</p>
+							<h5  class="mt_4">Expending Connectivity Across Different Vehicle Types</h5>
+							<p>The partnership between CANGO and MiTAC broadens connectivity to encompass a variety of vehicle types such as Heavy Goods Vehicles (HGVs) Light Commercial Vehicles (LCVs) Vans and pick ups, Trucks, Construction equipment, Mining machinery, Agricultural vehicles, Buses and Coaches Passenger Cars well as an extensive range of Electric vehicles (EVs) Hybrid models and Fuel Cell powered vehicles.</p>
+							<h5>Statements from CANGO and MITAC Representatives</h5>
+							<p  class="mt_4">“We are thrilled to join forces with MiTAC in delivering cutting edge video telematics solutions that empower fleet managers to make decisions based on data "said <span class="b">[CANGO Representative]</span>. "By combining MiTAC's dashcam technology with CANGO's comprehensive vehicle data insights, we are revolutionizing fleet management practices globally."</p>
+							<p  class="mt_4">"We have a strong belief in the collaboration between MDT and CANGO. Through this partnership, we aim to deliver a robust and highly integrated solution for the fleet management market. This initiative represents a forward-thinking approach to global expansion." said Steve Chang, President of MDT </p>
+						</div>
+					</div>
+
+					<p>
+						<br/><br/><br/>
+					</p>
+					<!--end-->
+					<div class='proj-nav'>
+						<div class='c-card-carousel__controller o-animate-in-element js-animate-in-element' style='opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);'>
+							<div class='proj-prev'>
+								<a href='press-events-list.php' target='_self'>
+									<div class='l-flex l-flex--center-middle c-card-carousel__prev' style='float: left;'>
+										<img src='../images/ic_chevron_left-white.svg' alt="arrow" class='o-icon c-card-carousel__arrow'>
+									</div>
+									<div class='proj-nav-title'><span>Back to Press &amp; Events</span></div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>		
+	</div>
+
+
+
+<?php
+	include 'inc/FOOTER.php';
+?>
